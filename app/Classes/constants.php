faaf641ade4977928793d17097d11b43ccc98d8f
sssteveCA <steppuggio@gmail.com>
2022-01-27 19:45:50 +0100

    Added sendFailedLoginResponse method to api/LoginController

--- app/Classes/constants.php
+++ app/Classes/constants.php
@@ -3,8 +3,11 @@
 //This class contains only constants messages
 interface Constants{
     //errors
+    const ERR_EMAILNOTFOUND = "Nessun account registrato con questa email";
+    const ERR_INVALIDCREDENTIALS = "Le credenziali inserite non sono valide";
     const ERR_NOTABLEGETUSERINFO = "Impossibile ottenere informazione sull'utente loggato";
     const ERR_PASSWORDINCORRECT = "La password attuale non è corretta";
+    const ERR_PASSWORDINCORRECTLOGIN = "La password inserita è errata";
     const ERR_URLNOTFOUND = "La pagina che hai richiesto non esiste";
 
     //success
--- app/Http/Controllers/api/Auth/LoginController.php
+++ app/Http/Controllers/api/Auth/LoginController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Constants;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    //Overriding method
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $response = array();
+        if(!User::where('email',$request->email)->first()){
+            //No account found with email entered
+            $response['errors'] = Constants::ERR_EMAILNOTFOUND;
+        }//if(!User::where('email',$request->email)->first()){
+        else if(!User::where('email',$request->email)->where('password',Hash::make($request->password))->first()){
+            //Incorrect password
+            $response['errors'] = Constants::ERR_PASSWORDINCORRECTLOGIN;
+        }//if(!User::where('email',$request->email)->where('password',Hash::make($request->password))->first()){
+        else{
+            //Other errors
+            $response['errors'] = Constants::ERR_INVALIDCREDENTIALS;
+        }
+            
+        return $response;
+    }
 }
