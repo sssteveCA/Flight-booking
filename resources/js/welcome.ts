58f57ee8d1d28573defd80827c60cf8c35f148d3
sssteveCA <steppuggio@gmail.com>
2022-07-04 16:50:43 +0200

    Added welcome.ts

---
+++ resources/js/welcome.ts
@@ -0,0 +1,9 @@
+
+$(()=>{
+    let buttons = $('button.nav-link');
+    buttons.on('click',(event)=>{
+         let clickbutton = event.currentTarget;
+         let cb_dbt = clickbutton.getAttribute('data-bs-target');
+         console.log(cb_dbt);
+    });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,10 @@
 <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 @endsection
 
+@section('scripts')
+<script src="{{ asset('js/welcome.js') }}"></script>
+@endsection
+
 @section('content')
 <div id="fb-content">
     <div class="fb-tab">
@@ -17,28 +21,55 @@
                 <button class="nav-link" id="events-tab" data-bs-toggle="tab" data-bs-target="#events" type="button" role="tab" aria-controls="events" aria-selected="false">Eventi</button>
             </div>
         </nav>
-        <div class="tab-content" id="nav-tabContent">
         <div class="tab-pane fade show active" id="tab-flights" role="tabpanel" aria-labelledby="tab-flights-tab">
             <form id="flightSearch" method="get" action="#">
-                <div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="roundtrip" id="fb-roundtrip">
-                        <label class="form-check-label" for="fb-roundtrip">Andata e ritorno</label>
+                <div class="container">
+                    <div id="fb-fs1" class="row">
+                        <div class="form-check col-6 col-md-2">
+                            <input class="form-check-input" type="radio" name="roundtrip" id="fb-roundtrip">
+                            <label class="form-check-label" for="fb-roundtrip">Andata e ritorno</label>
+                        </div>
+                        <div class="form-check col-6 col-md-2">
+                            <input class="form-check-input" type="radio" name="oneway" id="fb-oneway">
+                            <label class="form-check-label" for="fb-oneway">Solo andata</label>
+                        </div>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="oneway" id="fb-oneway">
-                        <label class="form-check-label" for="fb-oneway">Solo andata</label>
+                    <div id="fb-fs2" class="row">
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <input type="text" class="form-control" id="fb-from" name="from" placeholder="Partenza">
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <input type="text" class="form-control" id="fb-to" name="to" placeholder="Destinazione">
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-3">
+                            <button type="submit" class="btn btn-danger">Cerca</button>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <input type="text" class="form-control" id="fb-from" name="from" placeholder="Partenza">
-                    <input type="text" class="form-control" id="fb-to" name="to" placeholder="Destinazione">
-                    <button type="submit" class="btn btn-danger">Cerca</button>
                 </div>
             </form>
-            
         </div>
-        <div class="tab-pane fade" id="car-rental" role="tabpanel" aria-labelledby="car-rental-tab">Autonoleggio</div>
+        <div class="tab-pane fade" id="car-rental" role="tabpanel" aria-labelledby="car-rental-tab">
+            <div class="container">
+                <div class="row">
+                    <div id="fb-rc1" class="col-12">
+                        <input type="text" id="fb-rentcarloc" class="form-control-lg" name="rentcarloc" placeholder="Città o aereoporto per afitare l'auto">
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="fb-rc2" class="form-floating col-12 col-md-6 col-lg-5">
+                        <input type="datetime-local" id="fb-rentstart" class="form-control" name="rentstart" >
+                        <label for="fb-rentstart">Data di ritiro</label>
+                    </div>
+                    <div id="fb-rc3" class="form-floating col-12 col-md-6 col-lg-5">
+                        <input type="datetime-local" id="fb-rentend" class="form-control" name="rentend">
+                        <label for="fb-rentend">Data di consegna</label>
+                    </div>
+                    <div id="fb-rc4" class="col-12 col-lg-2">
+                        <button type="button" class="btn btn-danger">Andiamo</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="tab-pane fade" id="hotel" role="tabpanel" aria-labelledby="hotel-tab">Hotel</div>
         <div class="tab-pane fade" id="events" role="tabpanel" aria-labelledby="events-tab">Eventi</div>
         </div>
