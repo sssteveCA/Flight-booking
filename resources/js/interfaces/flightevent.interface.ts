e09839f75e60bbd800cef44f080b266ea38772a1
sssteveCA <steppuggio@gmail.com>
2022-07-04 21:36:41 +0200

    Removed unused json & ts files

--- resources/js/interfaces/flightevent.interface.ts
+++
@@ -1,7 +0,0 @@
-export default interface FlightEventInterface{
-    name: string;
-    location: string;
-    country: string;
-    price: number;
-    image: string;
-}
--- resources/js/welcome.ts
+++ resources/js/welcome.ts
@@ -1,5 +1,3 @@
-import FlightEventsList from "./classes/flighteventslist";
-
 $(()=>{
     let buttons = $('button.nav-link');
     buttons.on('click',(event)=>{
@@ -12,7 +10,6 @@ $(()=>{
          $('div[role=tabpanel]:not('+cb_dbt+')').css('display','none');
          if(cb_id == 'events-tab'){
             //User want see flight events list
-            let fel: FlightEventsList = new FlightEventsList();
          }//if(cb_id == 'events_tab'){
     });
 });
