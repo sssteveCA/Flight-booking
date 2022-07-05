7537f99690f502b4cbf2623d37ed4899132b2874
sssteveCA <steppuggio@gmail.com>
2022-07-05 23:47:17 +0200

    Added images in public/img/flightevents/

--- resources/js/values/constants.ts
+++ resources/js/values/constants.ts
@@ -1,4 +1,8 @@
 export namespace Constants{
+
+    export const FOLDER_IMG = '/img';
+    export const FOLDER_FLIGHTEVENTS = FOLDER_IMG+'/flightevents';
+    
     export const FOLDER_JSON = '/json';
     export const FOLDER_SCRIPT = '/scripts';
     export const HOSTNAME = 'http://127.0.0.1';
