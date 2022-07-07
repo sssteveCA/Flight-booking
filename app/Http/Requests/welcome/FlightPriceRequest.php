e91814937c5d31cee2d2df0bf978ee3bf2fabe6a
sssteveCA <steppuggio@gmail.com>
2022-07-07 18:12:41 +0200

    Added rles and messages method in welcome/FlightPriceRequest.php

--- app/Http/Requests/welcome/FlightPriceRequest.php
+++ app/Http/Requests/welcome/FlightPriceRequest.php
@@ -28,7 +28,30 @@ class FlightPriceRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'flight-type' => 'required',
+            'from' => 'required',
+            'from-airport' => 'required',
+            'to' => 'required',
+            'to-airport' => 'required',
+            'oneway-date' => 'required_without_all:roundtrip-start-date,roundtri-end-date',
+            'roundtrip-start-date' => 'required_with:roundtrip-end-date',
+            'roundtrip-end-date' => 'required_with:roundtrip-start-date',
+            'passengers' => 'required'
+
+        ];
+    }
+
+    public function messages(){
+        return [
+            'flight-type.required' => 'Seleziona il tipo di volo',
+            'from.required' => 'Seleziona il paese di provenienza',
+            'from-airport.required' => "Seleziona l'aereoporto di partenza",
+            'to.required' => 'Seleziona il paese di destinazione',
+            'to-airport.required' => "Seleziona l'aereoporto di destinazione",
+            'oneway-date.required_without_all' => 'Inserisci la data di partenza',
+            'rountrip-start-date.required_with' => 'Inserisci anche la data per il volo di ritorno',
+            'rountrip-end-date.required_with' => 'Inserisci anche la data per il volo di partenza',
+            'passengers.required' => 'Seleziona il numero di passeggeri'
         ];
     }
 }
