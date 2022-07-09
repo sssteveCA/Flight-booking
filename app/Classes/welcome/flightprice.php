9cef1977e99d60c49bfb2eddcb2e8aa01114e7e2
sssteveCA <steppuggio@gmail.com>
2022-07-09 15:24:08 +0200

    Updated properties in flightprice.php

--- app/Classes/welcome/flightprice.php
+++ app/Classes/welcome/flightprice.php
@@ -7,17 +7,67 @@ use Illuminate\Support\Facades\Log;
 
 //This class calculates the price of selected flight
 class FlightPrice{
-    private array $age_bands = array();
-    private array $aiports_list = array();
-    private float $days_before_discount;
-    private array $timetable_daily_bands = array();
-    private array $timetable_days = array();
-    private array $timetable_months = array();
+
+    public string $departure_country;
+    public string $arrival_country;
+    public string $departure_airport;
+    public float $departure_airport_lat;
+    public float $departure_airport_lon;
+    public string $arrival_airport;
+    public float $arrival_airport_lat;
+    public float $arrival_airport_lon;
+    public string $flight_date;
+    public string $flight_id;
+    public int $adults;
+    public int $teenagers;
+    public int $children;
+    public int $newborns;
+    public float $price;
+
+    public array $age_bands;
+    public array $airports_list;
+    public array $companies_list;
+    public float $days_before_discount;
+    public array $timetable_daily_bands;
+    public array $timetable_days;
+    public array $timetable_months;
+
     
     public function __construct(array $data)
     {
         
     }
+    
+    //Validate input data
+    private function validate(array $data):bool{
+        $valid = true;
+        return $valid;
+    }
+
+    //Assign input data to properties if all values are valid
+    private function setValues(array $data){
+        $this->departure_country = $data['departure_country'];
+        $this->arrival_country = $data['arrival_country'];
+        $this->departure_airport = $data['departure_airport'];
+        $this->departure_airport_lat = $data['departure_airport_lat'];
+        $this->departure_airport_lon = $data['departure_airport_lon'];
+        $this->arrival_airport = $data['arrival_airport'];
+        $this->arrival_airport_lat = $data['arrival_airport_lat'];
+        $this->arrival_airport_lon = $data['arrival_airport_lon'];
+        $this->flight_date = $data['flight_date'];
+        $this->adults = $data['adults'];
+        $this->teenagers = $data['teenagers'];
+        $this->children = $data['children'];
+        $this->newborns = $data['newborns'];
+        $this->age_bands = $data['age_bands'];
+        $this->airports_list = $data['airports_list'];
+        $this->companies_list = $data['companies_list'];
+        $this->days_before_discount = $data['days_before_discount'];
+        $this->timetable_daily_bands = $data['timetable_daily_bands'];
+        $this->timetable_days = $data['newborns'];
+        $this->timetable_months = $data['timetable_months'];
+    }
+
 
     public function __call($name, $arguments)
     {
@@ -26,14 +76,7 @@ class FlightPrice{
 
     public function __debugInfo()
     {
-        return [
-            'age_bands' => $this->age_bands,
-            'airports_list' => $this->airports_list,
-            'days_before_discount' => $this->days_before_discount,
-            'timetable_daily_bands' => $this->timetable_daily_bands,
-            'timetable_days' => $this->timetable_days,
-            'timetable_months' => $this->timetable_months,
-        ];
+
     }
 
     public function __get($name)
@@ -43,12 +86,21 @@ class FlightPrice{
 
     public function __set_state($properties)
     {
-        $this->age_bands = $properties['age_bands'];
-        $this->airports_list = $properties['airports_list'];
-        $this->days_before_discount = $properties['days_before_discount'];
-        $this->timetable_daily_bands = $properties['timetable_daily_bands'];
-        $this->timetable_days = $properties['timetable_days'];
-        $this->timetable_months = $properties['timetable_months'];
+        $this->departure_country = $properties['departure_country'];
+        $this->arrival_country = $properties['arrival_country'];
+        $this->departure_airport = $properties['departure_airport'];
+        $this->departure_airport_lat = $properties['departure_airport_lat'];
+        $this->departure_airport_lon = $properties['departure_airport_lon'];
+        $this->arrival_airport = $properties['arrival_airport'];
+        $this->arrival_airport_lat = $properties['arrival_airport_lat'];
+        $this->arrival_airport_lon = $properties['arrival_airport_lon'];
+        $this->flight_date = $properties['flight_date'];
+        $this->flight_id = $properties['flight_id'];
+        $this->adults = $properties['adults'];
+        $this->teenagers = $properties['teenagers'];
+        $this->children = $properties['children'];
+        $this->newborns = $properties['newborns'];
+        $this->price = $properties['price'];
         return $this;
     }
 
@@ -56,13 +108,6 @@ class FlightPrice{
     {
         return "Oggetto di tipo FlightPrice";
     }
-
-    public function getAgeBands(){return $this->age_bands;}
-    public function getAirports(){return $this->aiports_list;}
-    public function getDaysBeforeDiscount(){return $this->days_before_discount;}
-    public function getTtDailyBands(){return $this->timetable_daily_bands;}
-    public function getTtDays(){return $this->timetable_days;}
-    public function getTtMonths(){return $this->timetable_months;}
 
 }
 ?>
