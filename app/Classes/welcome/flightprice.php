8d05b9f25791d64e6522975f25ae787c166ac533
sssteveCA <steppuggio@gmail.com>
2022-07-09 15:44:13 +0200

    Added validate & setValues methods in flightprice.php

--- app/Classes/welcome/flightprice.php
+++ app/Classes/welcome/flightprice.php
@@ -41,6 +41,88 @@ class FlightPrice{
     //Validate input data
     private function validate(array $data):bool{
         $valid = true;
+        if(isset($data['departure_country'])){
+            if(trim($data['departure_country']) == '')$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['arrival_country'])){
+            if(trim($data['arrival_country']) == '')$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['departure_airport'])){
+            if(trim($data['departure_airport']) == '')$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['departure_airport_lat']) == ''){
+            if(!is_numeric($data['departure_airport_lat']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['departure_airport_lon']) == ''){
+            if(!is_numeric($data['departure_airport_lon']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['arrival_airport']) == ''){
+            if(trim($data['arrival_country']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['arrival_airport_lat'])){
+            if(!is_numeric($data['arrival_airport_lat']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['arrival_airport_lon'])){
+            if(!is_numeric($data['arrival_airport_lon']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['flight_date'])){
+            if(trim($data['flight_date']) == '')$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['adults'])){
+            if(!is_numeric($data['adults']))$valid = false;
+
+        }
+        else $valid = false;
+        if(isset($data['teenagers'])){
+            if(!is_numeric($data['teenagers']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['children'])){
+            if(!is_numeric($data['children']))$valid = false;
+
+        }
+        else $valid = false;
+        if(isset($data['newborns'])){
+            if(!is_numeric($data['newborns']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['age_bands'])){
+            if(!is_array($data['age_bands']) || sizeof($data['age_bands']) <= 0)$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['airports_list'])){
+            if(!is_array($data['airports_list']) || sizeof($data['airports_list']) <= 0)$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['companies_list'])){
+            if(!is_array($data['companies_list']) || sizeof($data['companies_list']) <= 0)$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['days_before_discount'])){
+            if(!is_numeric($data['days_before_discount']))$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['timetable_daily_bands'])){
+            if(!is_array($data['timetable_dailty_bands']) || sizeof($data['timetable_dailty_bands']) <= 0)$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['timetable_days'])){
+            if(!is_array($data['timetable_days']) || sizeof($data['timetable_days']) <= 0)$valid = false;
+        }
+        else $valid = false;
+        if(isset($data['timetable_months'])){
+            if(!is_array($data['timetable_months']) || sizeof($data['timetable_months']) <= 0)$valid = false;
+        }
+        else $valid = false;
         return $valid;
     }
 
