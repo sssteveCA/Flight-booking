5f6bc02561c594b1bbce8e38e89f7c25e815c6d4
sssteveCA <steppuggio@gmail.com>
2022-07-10 23:54:06 +0200

    Fixed variables print errors in flightpriceresult.blade.php

--- resources/views/welcome/flightpriceresult.blade.php
+++ resources/views/welcome/flightpriceresult.blade.php
@@ -12,13 +12,13 @@
     @isset($response['flight_type'])
         <form id="fFlight" method="post" action="#">
             @foreach($response['flights'] as $type => $flight)
-            <div class="flight">
+            <div class="flight-div">
                 @if($type == 'outbound')
-                <div class="flight-type">Volo di andata</div>
-                @else if($type == 'return')
-                <div class="flight-type">Volo di ritorno</div>
+                    <div class="flight-type">Volo di andata</div>
+                @elseif($type == 'return')
+                    <div class="flight-type">Volo di ritorno</div>
                 @else
-                <div class="flight-type">Volo di sola andata</div>
+                    <div class="flight-type">Volo di sola andata</div>
                 @endif
                 <div class="flight-row">
                     <div class="company column-elem">
@@ -27,15 +27,15 @@
                     </div>
                     <div class="date column-elem">
                         <p class="fl-header">Data e orario</p>
-                        <p>{{$flight['flight_date']  $flight['hours']}}</p>
+                        <p>{{$flight['flight_date'].' '.$flight['hours']}}</p>
                     </div>
                     <div class="departure-loc column-elem">
                         <p class="fl-header">Luogo di partenza</p>
-                        <p>{{$flight['departure_country'], $flight['departure_airport']}}</p>
+                        <p>{{$flight['departure_country'].', '.$flight['departure_airport']}}</p>
                     </div>
                     <div class="arrival-loc column-elem">
                         <p class="fl-header">Luogo di arrivo</p>
-                        <p>{{$flight['arrival_country'], $flight['arrival_airport']}}</p>
+                        <p>{{$flight['arrival_country'].', '.$flight['arrival_airport']}}</p>
                     </div>
                     <div class="price column-elem">
                         <p class="fl-header">Prezzo</p>
@@ -75,5 +75,4 @@
             <div class="alert alert-danger" role="alert">{{$error}}</div>
         @endforeach
     @endisset
-    
 @endsection
