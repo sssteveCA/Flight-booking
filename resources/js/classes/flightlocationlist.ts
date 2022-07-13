b2363ccb846ebd3df1c438d416e682a56a2c94da
sssteveCA <steppuggio@gmail.com>
2022-07-13 18:20:06 +0200

    Added constants errors codes & messages for flightlocationlist.ts

--- resources/js/classes/flightlocationlist.ts
+++ resources/js/classes/flightlocationlist.ts
@@ -2,13 +2,13 @@
 
 import FlightLocationCountriesInterface from "../interfaces/flightlocationcountries.interface";
 import FlightLocationAirportsInterface from "../interfaces/flightlocationairports.interface";
+import FlightLocationCompaniesInterface from "../interfaces/flightlocationcompanies.interface";
 import { Constants } from "../values/constants";
 
 export default class FlightLocationList{
     private _fired: JQuery<HTMLElement>;
     private _query: string;
     private _country: string; //Country which the airport is
-    private _company: string; //Flight company chosen
     private _selects: JQuery;
     private _id_companies_select: string; //select item id for company names list
     private _id_from_select: string; //select item id for departure countries list
@@ -16,9 +16,13 @@ export default class FlightLocationList{
     private _errno: number = 0;
     private _error: string|null = null;
 
-    public static ERR_FETCH:number = 1;
+    public static ERR_FETCH_COUNTRIES:number = 1;
+    public static ERR_FETCH_AIRPORTS:number = 2;
+    public static ERR_FETCH_COMPANIES:number = 3;
 
-    public static ERR_FETCH_MSG:string = "Errore durante l'esecuzione della richiesta";
+    public static ERR_FETCH_COUNTRIES_MSG:string = "Errore durante l'esecuzione della richiesta per la lista dei paesi";
+    public static ERR_FETCH_AIRPORTS_MSG:string = "Errore durante l'esecuzione della richiesta per la lista degli aereoporti del paese selezionato";
+    public static ERR_FETCH_COMPANIES_MSG:string = "Errore durante l'esecuzione della richiesta per la lista compagnie aeree";
 
     constructor(){  
     }
@@ -33,8 +37,14 @@ export default class FlightLocationList{
     get errno(){return this._errno;}
     get error(){
         switch(this._errno){
-            case FlightLocationList.ERR_FETCH:
-                this._error = FlightLocationList.ERR_FETCH_MSG;
+            case FlightLocationList.ERR_FETCH_COUNTRIES:
+                this._error = FlightLocationList.ERR_FETCH_COUNTRIES_MSG;
+                break;
+            case FlightLocationList.ERR_FETCH_AIRPORTS:
+                this._error = FlightLocationList.ERR_FETCH_AIRPORTS_MSG;
+                break;
+            case FlightLocationList.ERR_FETCH_COMPANIES:
+                this._error = FlightLocationList.ERR_FETCH_COMPANIES_MSG;
                 break;
             default:
                 this._error = null;
@@ -57,7 +67,7 @@ export default class FlightLocationList{
             if(this._id_to_select)this.set_airports(this._id_to_select,res);
             ok = true;
         }).catch(err => {
-            this._errno = FlightLocationList.ERR_FETCH;
+            this._errno = FlightLocationList.ERR_FETCH_AIRPORTS;
             console.warn(err);
         });
         return ok;
@@ -75,6 +85,12 @@ export default class FlightLocationList{
         return promise;
     }
 
+    public async get_flight_companies(data: FlightLocationCompaniesInterface): Promise<boolean>{
+        let ok = false;
+        this._errno = 0;
+        return ok;
+    }
+
     //Get available countries list 
     public async get_countries(data: FlightLocationCountriesInterface): Promise<boolean>{
         let ok = false;
@@ -88,7 +104,7 @@ export default class FlightLocationList{
             this.set_countries_select(this._id_to_select,res);
             ok = true;
         }).catch(err => {
-            this._errno = FlightLocationList.ERR_FETCH;
+            this._errno = FlightLocationList.ERR_FETCH_COUNTRIES;
             console.warn(err);
         });
         return ok;
