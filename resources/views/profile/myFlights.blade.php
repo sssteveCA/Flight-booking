466780f00dbd861818255aa640e925a05b1be16c
sssteveCA <steppuggio@gmail.com>
2022-07-15 00:28:35 +0200

    Added bootstrap grid system in myFlights.blade.php

--- resources/views/profile/myFlights.blade.php
+++ resources/views/profile/myFlights.blade.php
@@ -1,47 +1,48 @@
 @extends('layouts.menu')
 
+@section('namespaces')
+    @php
+        use App\Interfaces\Paths as P; 
+    @endphp
+@endsection
+
 @section('title','I miei voli')
 
 @section('content')
     @if($flights_number > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">NOME</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($flights as $flight)
-                <tr>
-                    <th scope="col">{{ $flight['id'] }}</th>
-                    <td>Da {{$flight['departure_airport']}} a {{$flight['arrival_airport']}}</td>
-                    <td>
-                        <form class="fFlightGet" method="get" action="#">
+        <div class="container-fluid">
+            @foreach($flights as $flight)
+                <div class="row justify-content-center justify-content-lg-start">
+                    <div class="col-2 col-lg-1 flight-id">
+                        <span class="flight-title-style">ID:</span> {{ $flight['id'] }}
+                    </div>
+                    <div class="col-10 col-lg-5 flight-name">
+                        <span class="flight-title-style">NOME</span> Da {{$flight['departure_airport']}} a {{$flight['arrival_airport']}}
+                    </div>   
+                    <div class="col-4 col-lg-2 flight-show">
+                        <form class="fFlightGet" method="get" action="{{ route('myFlights.show',['myFlight' => $flight['id']]) }}">
                             <button type="submit" class="btn btn-primary">VEDI</button>
                         </form>
-                    </td>
-                    <td>
+                    </div>
+                    <div class="col-4 col-lg-2 flight-delete">
                         <form class="fFlightDelete" method="post" action="#">
                             @csrf
                             @method('DELETE')
+                            <input type="hidden" name="flight_id" value="{{ $flight['id'] }}">
                             <button type="submit" class="btn btn-danger">ELIMINA</button>
                         </form>
-                    </td>
-                    <td>
-                        <form class="fFlightBook" method="post" action="#">
+                    </div>
+                    <div class="col-4 col-lg-2 flight-book">
+                        <form class="fFlightBook" method="post" action="{{ route(P::ROUTE_FLIGHTPRICE) }}">
                             @csrf
                             @method('POST')
+                            <!-- Informazioni sul volo da prenotare -->
                             <button type="submit" class="btn btn-success">PRENOTA</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                        </form>  
+                    </div>        
+                </div>
+            @endforeach
+        </div>
     @else
         @isset($message)
         <div class="container mt-5">
