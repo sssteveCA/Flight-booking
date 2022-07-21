e68c9605546a6f9a5ee0432a66b79dfd60e2770d
sssteveCA <steppuggio@gmail.com>
2022-07-21 12:58:23 +0200

    Added payment message status in return.blade.php

--- app/Http/Controllers/paypal/PaypalController.php
+++ app/Http/Controllers/paypal/PaypalController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Interfaces\Constants as C;
 use App\Interfaces\Paths as P;
+use App\Models\Flight;
+use DateTime;
+use DateTimeImmutable;
 use Illuminate\Support\Facades\Log;
 
 class PaypalController extends Controller
@@ -21,11 +24,33 @@ class PaypalController extends Controller
     //Return URL after user has made the payment
     public function return(Request $request){
         $post_data = $request->all();
-        
+        if($post_data['payer_status'] == "VERIFIED"){
+            //Payment completed
+            //Get number of payed items
+            $items_number = $post_data['num_cart_items'];
+            for($n = 1; $n <= $items_number; $n++){
+                $id = $post_data["item_number{$n}"];
+                $flight = Flight::find($id);
+                if($flight != null){
+                    //Found record with provided id
+                    $flight->payed = 1;
+                    $datetime = new DateTime($post_data['payment_date']);
+                    $flight->payed_date = $datetime->format('Y-m-d H:i:s');
+                    $flight->transaction_id = $post_data['txn_id'];
+                    $flight->save();
+                }//if($flight != null){
+            }
+            $message = C::OK_FLIGHTPAYMENT;
+            $payment = 'completed';
+        }//if($post_data['status'] == "VERIFIED"){
+        else{
+            $message = C::ERR_FLIGHTPAYMENT_REFUSE;
+            $payment = 'refused';
+        }
         Log::channel('stdout')->debug("PaypalController return post_data => ".var_export($post_data,true));
         return response()->view(P::VIEW_PAYPAL_RETURN,[
-            'payment' => 'completed',
-            'message' => C::OK_FLIGHTPAYMENT,
+            'payment' => $payment,
+            'message' => $message,
             'post_data' => $post_data
         ],200);
     }
--- resources/views/paypal/return.blade.php
+++ resources/views/paypal/return.blade.php
@@ -1,11 +1,22 @@
 @extends('layouts.menu')
 
 @section('content')
-    @isset($post_data)
+    {{-- @isset($post_data)
         @php
             echo '<pre>';
             var_dump($post_data);
             echo '</pre>';
         @endphp
+    @endisset
+    --}}
+    @isset($payment)
+        <div class="container mt-5">
+            <h2 class="mt-5 text-center">Prenotazione volo</h2>
+            @if($payment == 'completed')
+            <p class="lead text-center">{{ $message }}</p>
+            @else if($payment == 'refused')
+            <div class="alert alert-danger" role="alert">{{ $message }}</div>
+            @endif
+        </div>
     @endisset
 @endsection
