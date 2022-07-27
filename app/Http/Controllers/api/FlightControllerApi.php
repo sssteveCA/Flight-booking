13f387b1458644af07d0a54877eb3aedb2f7d9b5
sssteveCA <steppuggio@gmail.com>
2022-07-27 22:18:35 +0200

    Before creating AuthenticateApi middleware

--- app/Http/Controllers/api/FlightControllerApi.php
+++ app/Http/Controllers/api/FlightControllerApi.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Flight;
 use Illuminate\Http\Request;
+use App\Interfaces\Constants as C;
+use Illuminate\Support\Facades\Log;
 
 class FlightControllerApi extends Controller
 {
@@ -14,7 +17,28 @@ class FlightControllerApi extends Controller
      */
     public function index()
     {
-        //
+        Log::channel('stdout')->info("FlightControllerApi index");
+        $user_id = auth()->id();
+        //$flights_number = Flight::where('user_id',$user_id)->count();
+        $flights_collection = Flight::where('user_id',$user_id)->get();
+        $flights = $flights_collection->toArray();
+        //Log::channel('stdout')->debug("User flights => ".var_export($flights,true));
+        $flights_number = count($flights);
+        if($flights_number > 0){
+            //User has booked at least one flight
+            return response()->json([
+                'flights' => $flights,
+                'flights_number' => $flights_number
+            ],200,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }//if($flights_number > 0){
+        else{
+            //User has not booked any flight already
+            $message = C::MESS_BOOKED_FLIGHT_LIST_EMPTY;
+            return response()->json([
+                'message' => $message,
+                'flights_number' => $flights_number
+            ],200,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);           
+        }    
     }
 
     /**
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -19,25 +19,33 @@ class Authenticate extends Middleware
      protected function redirectTo($request)
     {
         Log::channel('stdout')->info("Authenticate redirectTo");
-        $route_params = [];
-        if($request->isMethod('POST')){
-            //Method of request is POST
-            if($request->routeIs(P::ROUTE_BOOKFLIGHT)){
-                //Route name when flight prices are shown
-                $params = $request->input('flights');
-                $route_params = $params;
-                //$route_params = $this->flights_unquote($params);
-                //Log::channel('stdout')->info("Authenticate redirectTo request => ".var_export($route_params,true));
-                $route_params = [
-                    'flight_type' => $request->input('flight_type'),
-                    'flights' => $route_params];
-                $request->flash();
-            }//if($request->routeIs(P::ROUTE_FLIGHTPRICE)){
-        }//if($request->isMethod('post')){
-        if (! $request->expectsJson()) {
-            //return route('login',['flights' => $request->all()]);
-            return route('login',$route_params);
-        }
+        $path = $request->path();
+        Log::channel('stdout')->info("Authenticate redirectTo path => ".var_export($path,true));
+        $api_request = str_starts_with($path,'api/');
+        Log::channel('stdout')->info("Authenticate redirectTo str starts with => ".var_export($api_request,true));
+        if($api_request === false){ 
+            Log::channel('stdout')->info("Authenticate redirectTo api_request");
+            //Log::channel('stdout')->info("Authenticate redirectTo host => ".var_export($request,true));
+            $route_params = [];
+            if($request->isMethod('POST')){
+                //Method of request is POST
+                if($request->routeIs(P::ROUTE_BOOKFLIGHT)){
+                    //Route name when flight prices are shown
+                    $params = $request->input('flights');
+                    $route_params = $params;
+                    //$route_params = $this->flights_unquote($params);
+                    //Log::channel('stdout')->info("Authenticate redirectTo request => ".var_export($route_params,true));
+                    $route_params = [
+                        'flight_type' => $request->input('flight_type'),
+                        'flights' => $route_params];
+                    $request->flash();
+                }//if($request->routeIs(P::ROUTE_FLIGHTPRICE)){
+            }//if($request->isMethod('post')){
+            if (! $request->expectsJson()) {
+                //return route('login',['flights' => $request->all()]);
+                return route('login',$route_params);
+            }
+        }//if(!$api_request){ 
     } 
 
     //Remove backslashes from flights array keys
