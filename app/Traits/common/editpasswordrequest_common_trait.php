baac6ba7b0a8429c9023c952e1d2e11d8eae0655
sssteveCA <steppuggio@gmail.com>
2022-07-28 15:57:51 +0200

    Added editpasswordrequest_common_trait.php

---
+++ app/Traits/common/editpasswordrequest_common_trait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits\Common;
+
+//This trait contains common code for EditPasswordRequest & EditPasswordRequestApi
+trait EditPasswordRequestCommonTrait{
+    
+    public function attributes()
+    {
+        return [
+            'oldpwd' => 'vecchia password',
+            'newpwd' => 'nuova password',
+            'confnewpwd' => 'conferma nuova password'
+        ];
+    }
+
+    public function messages()
+    {
+        //Validation error messages
+        return [
+            'required' => 'Il campo :attribute è obbligatorio',
+            'min' => 'Il campo :attribute deve avere almeno :min caratteri',
+            'confnewpwd.same' => ':attribute deve essere uguale al campo nuova password',
+            'oldpwd.password' => 'Password attuale errata',                 
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'oldpwd' => ['required','password'],
+            'newpwd' => ['required','min:8'],
+            'confnewpwd' => ['required','min:8','same:newpwd']
+        ];
+    }
+}
+?>
