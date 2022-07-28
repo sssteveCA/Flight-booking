63190b4f261d0ffadb2692eeb9932c885d7c1be9
sssteveCA <steppuggio@gmail.com>
2022-07-28 20:42:34 +0200

    Added CheckAirports.php rule

---
+++ app/Rules/CheckAirports.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Rules;
+
+use App\Traits\FlightSearchTrait;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Log;
+
+//Check if airports value is inside specific array
+class CheckAirports implements Rule,DataAwareRule
+{
+    use FlightSearchTrait;
+
+    private string $what_field; //Country field when check
+    private $field_value; //what_field value
+    protected $data;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(string $what_field)
+    {
+        $this->what_field = $what_field;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        Log::channel('stdout')->info('CheckAirports what_field => '.var_export($this->what_field,true));
+        Log::channel('stdout')->info('CheckAirports field value => '.var_export($this->field_value,true));
+        $airports = $this->getAirportsList($this->field_value);
+        Log::channel('stdout')->info('CheckAirports airports => '.var_export($airports,true));
+        return in_array($value,$airports);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Il valore di :attribute non è presente tra quelli possibili';
+    }
+
+    public function setData($data)
+    {
+        $this->field_value = $data[$this->what_field];
+        return $this;
+    }
+}
