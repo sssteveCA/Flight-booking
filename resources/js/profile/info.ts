9cb907b67e94ab232ad22114714d757c198803d2
sssteveCA <steppuggio@gmail.com>
2022-07-29 15:18:28 +0200

    Added showPassword, editUsernameForm, editPasswordForm functions in info.ts

--- resources/js/profile/info.ts
+++ resources/js/profile/info.ts
@@ -5,10 +5,18 @@ import MessageDialog from "../classes/dialog/messagedialog";
 import { Constants } from "../values/constants";
 import EditUsername from "../classes/profile/editusername";
 import EditUsernameInterface from "../interfaces/profile/editusername.interface";
+import EditPasswordInterface from "../interfaces/profile/editpassword.interface";
+import EditPassword from "../classes/profile/editpassword";
 
 $(()=>{
-     //detect showPassword checkbox changes
-     $('#showPassword').on('change',function(){
+    showPassword();
+    editUsernameForm();
+    editPasswordForm();
+});
+
+function showPassword(): void{
+    //detect showPassword checkbox changes
+    $('#showPassword').on('change',function(){
         //console.log("ShowPassword change");
         var checked = $(this).is(":checked");
         if(checked){
@@ -22,6 +30,9 @@ $(()=>{
             $('#confnewpwd').attr('type','password');
         }
     });
+}
+
+function editUsernameForm(): void{
     $('#fEditUsername').on('submit',(e: JQuery.Event)=>{
         //Edit username form submitted
         e.preventDefault();
@@ -48,15 +59,16 @@ $(()=>{
                     md.dialog.dialog('destroy');
                     md.dialog.remove();
                 });
-            }).catch(err => {
-
             });
-        });
+        });//cd.btYes.on('click', ()=>{
         cd.btNo.on('click',()=>{
             cd.dialog.dialog('destroy');
             cd.dialog.remove();
         });
     });
+}
+
+function editPasswordForm(): void{
     $('#fEditPassword').on('submit',(e: JQuery.Event)=> {
         //Edit password form submitted
         e.preventDefault();
@@ -68,10 +80,28 @@ $(()=>{
         cd.btYes.on('click', ()=>{
             cd.dialog.dialog('destroy');
             cd.dialog.remove();
-        });
+            let ep_data: EditPasswordInterface = {
+                oldpwd: $('#oldpwd').val() as string,
+                newpwd: $('#newpwd').val() as string,
+                confnewpwd: $('#confnewpwd').val() as string,
+                token: $('#fEditPassword input[name=_token]').eq(0).val() as string
+            };
+            let editPassword: EditPassword = new EditPassword(ep_data);
+            editPassword.editPassword().then(res => {
+                let md_data: MessageDialogInterface = {
+                    title: 'Modifica password',
+                    message: res
+                };
+                let md: MessageDialog = new MessageDialog(md_data);
+                md.btOk.on('click', ()=>{
+                    md.dialog.dialog('destroy');
+                    md.dialog.remove();
+                });
+            });
+        });//cd.btYes.on('click', ()=>{
         cd.btNo.on('click',()=>{
             cd.dialog.dialog('destroy');
             cd.dialog.remove();
         });
     });
-});
+}
