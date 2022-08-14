d1ce365a077b0b8c21a428228aa545489309c47a
sssteveCA <steppuggio@gmail.com>
2022-08-14 21:36:02 +0200

    Added app/Interfaces/welcome/flighttempmanager_errors.php

--- app/Classes/welcome/flighttempmanager.php
+++ app/Classes/welcome/flighttempmanager.php
@@ -4,8 +4,10 @@ namespace App\Classes\Welcome;
 
 use App\Traits\ErrorTrait;
 use App\Traits\MmCommonTrait;
+use App\Exceptions\FlightArrayException;
+use App\Interfaces\Welcome\FlightTempManagerErrors as Ftme;
 
-class FlightTempManager{
+class FlightTempManager implements Ftme{
     use ErrorTrait, MmCommonTrait;
 
     private array $flights_array;
@@ -13,6 +15,19 @@ class FlightTempManager{
     public function __construct(array $data)
     {
         
+    }
+
+    public function getFlightsArray(){return $this->flights_array;}
+
+    //check if flights array is valid
+    private function checkFlightArray(array $data){
+        $count = count($data);
+        if($count > 0 && $count <= 2){
+
+        }//if($count > 0 && $count <= 2){
+        else{
+            throw new FlightArrayException(Ftme::FLIGHTARRAY_EXC,1);
+        }
     }
 }
 ?>
---
+++ app/Interfaces/welcome/flighttempmanager_errors.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Interfaces\Welcome;
+
+interface FlightTempManagerErrors{
+    //Exceptions
+    const FLIGHTARRAY_EXC = "L' array che contiene i voli prenotati non è formattato correttamente";
+}
+?>
