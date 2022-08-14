d5c2fa9365e5e0dcfa635c8f96f334084a2d8a22
sssteveCA <steppuggio@gmail.com>
2022-08-15 00:21:31 +0200

    Added checkFlightSerchRequests method in flightstempmanager_common_trait.php

--- app/Classes/welcome/flighttempmanager.php
+++ app/Classes/welcome/flighttempmanager.php
@@ -11,12 +11,12 @@ use App\Traits\Common\FlightsTempManagerCommonTrait;
 class FlightTempManager implements Ftme{
     use ErrorTrait, MmCommonTrait, FlightsTempManagerCommonTrait;
 
-    public function __construct(array $data)
+
+    public function __construct(array $data,string $session_id)
     {
         $this->checkFlightsArray($data);
         $this->flights_array = $data;
     }
-
 
 }
 ?>
--- app/Traits/common/flightstempmanager_common_trait.php
+++ app/Traits/common/flightstempmanager_common_trait.php
@@ -5,11 +5,14 @@ namespace App\Traits\Common;
 use App\Exceptions\FlightArrayException;
 use App\Classes\Welcome\FlightTempManager;
 use App\Interfaces\Welcome\FlightTempManagerErrors as Ftme;
+use App\Models\FlightTemp;
 
 //Common code between flightstempmanager.php & flightstempmanagerapi.php
 trait FlightsTempManagerCommonTrait{
 
+    private FlightTemp $flight_temp;
     private array $flights_array;
+    private string $session_id;
 
     private static array $flight_directions = ['oneway','outbound','return'];
     private static array $flight_properties = ['company_name','departure_country','departure_airport','booking_date',
@@ -17,6 +20,7 @@ trait FlightsTempManagerCommonTrait{
     'total_price' ];
 
     public function getFlightsArray(){return $this->flights_array;}
+    public function getSessionId(){return $this->session_id;}
 
     //check if flights array is valid
     private function checkFlightsArray(array $data){
@@ -37,6 +41,18 @@ trait FlightsTempManagerCommonTrait{
         else{
             throw new FlightArrayException(Ftme::FLIGHTARRAY_EXC,1);
         }
+    }
+
+    //Check if user with session id has shortly before done a flight search
+    private function checkFlightSearchRequests(): bool{
+        $exists = false;
+        $flights = FlightTemp::where('session_id',$this->session_id)->get();
+        //Get the number of items in the collection
+        if($flights->count() > 0){
+            //user has done shortly before a flight search
+            $exists = true;
+        }//if($flights->count() > 0){
+        return $exists;
     }
 
 }
