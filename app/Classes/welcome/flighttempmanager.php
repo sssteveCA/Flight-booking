e2ec70914b100049c6a898f0632326b337499995
sssteveCA <steppuggio@gmail.com>
2022-08-14 21:18:48 +0200

    Added app/Classes/welcome/flighttempmanager.php

---
+++ app/Classes/welcome/flighttempmanager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Classes\Welcome;
+
+use App\Traits\ErrorTrait;
+use App\Traits\MmCommonTrait;
+
+class FlightTempManager{
+    use ErrorTrait, MmCommonTrait;
+
+    public function __construct(array $data)
+    {
+        
+    }
+}
+?>
