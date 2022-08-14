562c9587ba7bf55dfa6cf8482573dd999c799f99
sssteveCA <steppuggio@gmail.com>
2022-08-14 21:49:59 +0200

    Added checkFlightsArray method in flighttempmanager.php

--- app/Classes/welcome/flighttempmanager.php
+++ app/Classes/welcome/flighttempmanager.php
@@ -12,18 +12,36 @@ class FlightTempManager implements Ftme{
 
     private array $flights_array;
 
+    private static array $flight_directions = ['oneway','outbound','return'];
+    private static array $flight_properties = ['company_name','departure_country','departure_airport','booking_date',
+    'flight_date','flight_time','arrival_country','arrival_airport','adults','teenagers','children','newborns',
+    'total_price' ];
+
+
     public function __construct(array $data)
     {
-        
+        $this->checkFlightsArray($data);
+        $this->flights_array = $data;
+
     }
 
     public function getFlightsArray(){return $this->flights_array;}
 
     //check if flights array is valid
-    private function checkFlightArray(array $data){
+    private function checkFlightsArray(array $data){
         $count = count($data);
         if($count > 0 && $count <= 2){
-
+            foreach($data as $direction => $flight){
+                if(in_array($direction,FlightTempManager::$flight_directions)){
+                    foreach($flight as $key => $value){
+                        if(!in_array($key,FlightTempManager::$flight_properties)){
+                            throw new FlightArrayException(Ftme::FLIGHTARRAY_EXC);
+                        }
+                    }//foreach($flight as $key => $value){
+                }//if(in_array($direction,FlightTempManager::$flight_directions)){
+                else
+                    throw new FlightArrayException(Ftme::FLIGHTARRAY_EXC,1);
+            }//foreach($data as $direction => $flight){
         }//if($count > 0 && $count <= 2){
         else{
             throw new FlightArrayException(Ftme::FLIGHTARRAY_EXC,1);
