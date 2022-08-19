507931adb01fcd5e34f0d442dfadc166fadba017
Acer <Acer@DESKTOP-K09QF87>
2022-08-19 22:24:53 +0200

    Added /config.ts

---
+++ config.ts
@@ -0,0 +1,8 @@
+
+//Set these variables depending on your environment
+export namespace Config{
+    export const SCHEME = 'http';
+    export const HOSTNAME = '127.0.0.1';
+    export const PORT = 8000;
+    export const HOME_URL = SCHEME+'://'+HOSTNAME+':'+PORT;
+}
