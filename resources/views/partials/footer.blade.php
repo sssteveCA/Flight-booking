5aa290573642cbb9c604a09308e6fb404ca00b97
Acer <Acer@DESKTOP-K09QF87>
2022-10-06 19:03:45 +0200

    Added project information in footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,19 @@
 <div class="footer">
-    <div>
-    © 2022 - Stefano Puggioni
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="not-custom-font col-12 col-lg-10">
+                Flight Booking è un'applicazione web che ti permette di prenotare voli da più compagnie in pochi passi. E' sufficiente registrarsi gratuitamente alla piattaforma.
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="not-custom-font col-12 col-lg-10">
+                Questa applicazione è sviluppata da <a href="https://github.com/sssteveCA/Flight-booking">Stefano Puggioni</a>.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-lg-10 text-center">
+                © 2022 - FlightBooking
+            </div>
+        </div>
     </div>
 </div>
