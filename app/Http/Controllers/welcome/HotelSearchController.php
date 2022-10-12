42f328bb2a7aec308fddf8bca49a90cabe85e7a8
Acer <Acer@DESKTOP-K09QF87>
2022-10-12 23:24:18 +0200

    Added getCitiesList, getCountriesList and getHotelsList in hotelsearchcontroller_common_trait.php

--- app/Http/Controllers/welcome/HotelSearchController.php
+++ app/Http/Controllers/welcome/HotelSearchController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\welcome;
 
 use App\Http\Controllers\Controller;
+use App\Traits\Common\HotelSearchControllerCommonTrait;
 use Illuminate\Http\Request;
 
 class HotelSearchController extends Controller
 {
-    //
+    use HotelSearchControllerCommonTrait;
 }
--- app/Traits/common/hotelsearchcontroller_common_trait.php
+++ app/Traits/common/hotelsearchcontroller_common_trait.php
@@ -2,10 +2,43 @@
 
 namespace App\Traits\Common;
 
+use App\Interfaces\Hotels as H;
+
 /**
  * This trait is used to store common code in HotelSearchController & HotelSearchControllerApi
  */
 trait HotelSearchControllerCommonTrait{
-    
+
+
+    /**
+     * Get the cities of a country that contains hotels list
+     */
+    protected function getCitiesList(string $country): array{
+        if(isset(H::HOTELS_LIST[$country]))
+            return array_keys(H::HOTELS_LIST[$country]);
+        return [];
+    }
+
+    /**
+     * Get the countries list that contains hotels
+     */
+    protected function getCountriesList(): array{
+        return array_keys(H::HOTELS_LIST);
+    }
+
+    /**
+     * Get the hotels list of a city's country
+     */
+    protected function getHotelsList(string $country, string $city): array{
+        if(isset(H::HOTELS_LIST[$country])){
+            if(isset(H::HOTELS_LIST[$country][$city])){
+                $hotels_list = H::HOTELS_LIST[$country][$city];
+                return array_keys($hotels_list);
+            }
+        }
+        return [];
+    }
+
+
 }
 ?>
