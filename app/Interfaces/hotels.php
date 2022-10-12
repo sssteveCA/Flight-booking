ad8008fd16bc3a308eb77e4daaa53a14017d92bd
Acer <Acer@DESKTOP-K09QF87>
2022-10-12 19:18:14 +0200

    Added countries to HOTELS_LIST array in hotels.php

--- app/Interfaces/hotels.php
+++ app/Interfaces/hotels.php
@@ -3,6 +3,43 @@
 namespace App\Interfaces;
 
 interface Hotels{
-    
+    const HOTELS_LIST = [
+        'Austria' => [],
+        'Belgio' => [],
+        'Bosnia & Herzegovina' => [],
+        'Bulgaria' => [],
+        'Cipro' => [],
+        'Croazia' => [],
+        'Danimarca' => [],
+        'Estonia' => [],
+        'Finlandia' => [],
+        'Francia' => [],
+        'Germania' => [],
+        'Giordania' => [],
+        'Grecia' => [],
+        'Irlanda' => [],
+        'Israele' => [],
+        'Italia' => [],
+        'Lettonia' => [],
+        'Lituania' => [],
+        'Lussemburgo' => [],
+        'Malta' => [],
+        'Marocco' => [],
+        'Montenegro' => [],
+        'Norvegia' => [],
+        'Olanda' => [],
+        'Polonia' => [],
+        'Portogallo' => [],
+        'Regno Unito' => [],
+        'Repubblica Ceca' => [],
+        'Romania' => [],
+        'Serbia' => [],
+        'Slovacchia' => [],
+        'Spagna' => [],
+        'Svezia' => [],
+        'Svizzera' => [],
+        'Turchia' => [],
+        'Ungheria' => [],
+    ];
 }
 ?>
