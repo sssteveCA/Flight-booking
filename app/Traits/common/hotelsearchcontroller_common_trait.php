09570e77d949e1a7c71c8247363c2b2a6f100ce2
Acer <Acer@DESKTOP-K09QF87>
2022-10-12 23:38:29 +0200

    Added getCities, getCountries and getHotels in hotelsearchcontroller_common_trait.php

--- app/Traits/common/hotelsearchcontroller_common_trait.php
+++ app/Traits/common/hotelsearchcontroller_common_trait.php
@@ -3,32 +3,59 @@
 namespace App\Traits\Common;
 
 use App\Interfaces\Hotels as H;
+use Illuminate\Http\Request;
 
 /**
  * This trait is used to store common code in HotelSearchController & HotelSearchControllerApi
  */
 trait HotelSearchControllerCommonTrait{
 
+    /**
+     * Get the countries list that contains hotels
+     */
+    public function getCountries(Request $request){
+        $countries = $this->getCountriesList();
+        return response()->json($countries,200,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+    }
+
 
     /**
      * Get the cities of a country that contains hotels list
      */
+    public function getCities(Request $request){
+        $country = $request->query('country','');
+        $cities = $this->getCitiesList($country);
+        if(sizeof($cities) > 0)
+            $response_code = 200;
+        else
+            $response_code = 404;
+        return response()->json($cities,$response_code,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Get the hotels list of a city's country
+     */
+    public function getHotels(Request $request){
+        $country = $request->query('country','');
+        $city = $request->query('city','');
+        $hotels = $this->getHotelsList($country,$city);
+        if(sizeof($hotels) > 0)
+            $response_code = 200;
+        else
+            $response_code = 404;
+        return response()->json($hotels,$response_code,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+    }
+
     protected function getCitiesList(string $country): array{
         if(isset(H::HOTELS_LIST[$country]))
             return array_keys(H::HOTELS_LIST[$country]);
         return [];
     }
 
-    /**
-     * Get the countries list that contains hotels
-     */
     protected function getCountriesList(): array{
         return array_keys(H::HOTELS_LIST);
     }
 
-    /**
-     * Get the hotels list of a city's country
-     */
     protected function getHotelsList(string $country, string $city): array{
         if(isset(H::HOTELS_LIST[$country])){
             if(isset(H::HOTELS_LIST[$country][$city])){
