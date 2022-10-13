d7b8f864bbca722438c3c17b46b56f70b119eef4
Acer <Acer@DESKTOP-K09QF87>
2022-10-13 16:15:27 +0200

    Added resources/js/welcome.hotels.ts

---
+++ resources/js/welcome.hotel.ts
@@ -0,0 +1,19 @@
+import HotelCountries from "./classes/hotel/hotelcountries";
+import HotelCountriesInterface from "./interfaces/hotel/hotelcountries.interface";
+
+$(()=>{
+    loadHotelData();
+});
+
+function loadHotelData(): void{
+    let hc_data: HotelCountriesInterface = {
+        select_id: 'hotelCountries'
+    };
+    let hc: HotelCountries = new HotelCountries(hc_data);
+    hc.get_hotel_countries().then(countries => {
+        console.log("countries");
+        console.log(countries);
+    }).catch(err => {
+
+    });
+}
