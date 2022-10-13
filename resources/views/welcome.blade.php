07047261df7e0c1a522f0cab6675682399f75e54
Acer <Acer@DESKTOP-K09QF87>
2022-10-13 17:22:17 +0200

    Added countries, cities and hotels select dropdown to hotel-tab in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -135,24 +135,33 @@
         <div class="tab-pane fade" id="hotel" role="tabpanel" aria-labelledby="hotel-tab">
             <div class="container">
                 <div class="row">
-                    <div id="fb-h1" class="col-12">
-                        <input type="text" id="fb-hotelloc" class="form-control-lg" name="hotelloc" placeholder="Scegli la città in cui vuoi avere l'albergo">
+                    <div id="fb-h1" class="form-floating col-12 col-md-6 col-lg-4">
+                        <select id="hotelCountries" class="form-select form-select-lg"></select>
+                        <label for="hotelCountries">Paese</label>
+                    </div>
+                    <div id="fb-h2" class="form-floating col-12 col-md-6 col-lg-4">
+                        <select id="hotelCities" class="form-select form-select-lg"></select>
+                        <label for="hotelCities">Città</label>
+                    </div>
+                    <div id="fb-h3" class="form-floating col-12 col-md-6 col-lg-4">
+                        <select id="hotelsList" class="form-select form-select-lg"></select>
+                        <label for="hotelsList">Albergo</label>
                     </div>
                 </div>
                 <div class="row">
-                    <div id="fb-h2" class="form-floating col-12 col-md-6 col-lg-3">
+                    <div id="fb-h4" class="form-floating col-12 col-md-6 col-lg-3">
                         <input type="date" id="fb-checkin" class="form-control" name="checkin" >
                         <label for="fb-checkin">Check-in</label>
                     </div>
-                    <div id="fb-h3" class="form-floating col-12 col-md-6 col-lg-3">
+                    <div id="fb-h5" class="form-floating col-12 col-md-6 col-lg-3">
                         <input type="date" id="fb-checkout" class="form-control" name="checkout">
                         <label for="fb-checkout">Check-out</label>
                     </div>
-                    <div id="fb-h4" class="form-floating col-12 col-md-6 col-lg-3">
+                    <div id="fb-h6" class="form-floating col-12 col-md-6 col-lg-3">
                         <input type="text" id="fb-rooms" class="form-control" name="rooms">
                         <label for="fb-rooms">Ospiti e camere</label>
                     </div>
-                    <div id="fb-h5" class="col-12 col-md-6 col-lg-2">
+                    <div id="fb-h7" class="col-12 col-md-6 col-lg-2">
                         <button type="submit" class="btn btn-danger">Cerca</button>
                     </div>
                 </div>
