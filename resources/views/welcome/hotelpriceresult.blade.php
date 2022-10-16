8a9fd9344426c7d22fee592f43cefa96322f1fee
Acer <Acer@DESKTOP-K09QF87>
2022-10-16 21:43:27 +0200

    Added resources/views/welcome/hotelpriceresult.blade.php

---
+++ resources/views/welcome/hotelpriceresult.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.menu')
+
+@section('namespaces')
+@endsection
+
+@section('title','Prezzo albergo scelto')
+
+@section('links')
+@endsection
+
+@section('scripts')
+@endsection
+
+@section('content')
+@endsection
