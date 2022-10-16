46d2073e4b95cdd0d3c17acba7e9eb3b3dd3a642
Acer <Acer@DESKTOP-K09QF87>
2022-10-16 22:05:46 +0200

    Added app/Http/Requests/welcome/HotelPriceRequest.php

--- app/Http/Controllers/welcome/HotelSearchController.php
+++ app/Http/Controllers/welcome/HotelSearchController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\welcome;
 use App\Http\Controllers\Controller;
 use App\Traits\Common\HotelSearchControllerCommonTrait;
 use Illuminate\Http\Request;
+use App\Interfaces\Paths as P;
 
 class HotelSearchController extends Controller
 {
     use HotelSearchControllerCommonTrait;
 
     public function getHotelPrice(Request $request){
-        
+        return response()->view(P::VIEW_HOTELPRICERESULT,[]);
     }
 }
