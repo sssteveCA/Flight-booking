58992166528d993f123bdba4e97f7e51acd04723
Acer <Acer@DESKTOP-K09QF87>
2022-10-18 23:22:30 +0200

    Added calcPreventive method in hotelprice.php

--- app/Classes/welcome/hotelprice.php
+++ app/Classes/welcome/hotelprice.php
@@ -2,14 +2,16 @@
 
 namespace App\Classes\Welcome;
 
+use App\Exceptions\HotelPriceRequestArrayException;
 use App\Traits\ErrorTrait;
 use App\Traits\MmCommonTrait;
 use App\Interfaces\Hotels as H;
 use App\Interfaces\Welcome\HotelPriceErrors as Hpe;
+use App\Traits\DateTrait;
 use App\Traits\HotelPriceTrait;
 
 class HotelPrice implements Hpe{
-    use ErrorTrait, MmCommonTrait,HotelPriceTrait;
+    use DateTrait, ErrorTrait, MmCommonTrait,HotelPriceTrait;
 
     /**
      * The country which the hotel is situated
@@ -26,11 +28,11 @@ class HotelPrice implements Hpe{
     /**
      * The date of the check-in
      */
-    private string $checkinDate;
+    private string $checkin;
     /**
      * The date of the check-out
      */
-    private string $checkoutDate;
+    private string $checkout;
     /**
      * The number of people that booked rooms in a hotel
      */
@@ -40,27 +42,47 @@ class HotelPrice implements Hpe{
      */
     private int $rooms;
 
-    private array $hotelPriceInputs;
-
     private float $fullPrice;
 
     public function __construct(array $data)
     {
         $this->assignValues($data);
+        $this->calcPreventive();
     }
 
     public function getCountry(){return $this->country;}
     public function getCity(){return $this->city;}
     public function getHotel(){return $this->hotel;}
-    public function getCheckinDate(){return $this->checkinDate;}
-    public function getCheckoutDate(){return $this->checkoutDate;}
+    public function getCheckinDate(){return $this->checkin;}
+    public function getCheckoutDate(){return $this->checkout;}
     public function getPeople(){return $this->people;}
     public function getRooms(){return $this->rooms;}
     public function getFullPrice(){return $this->fullPrice;}
-    public function getHotelPriceInputs(){return $this->hotelPriceInputs;}
+    public function getError(){
+        switch($this->errno){
+            case Hpe::HOTELINFO_NOTFOUND:
+                $this->error = Hpe::HOTELINFO_NOTFOUND_MSG;
+                break;
+            case Hpe::TOOMANYPEOPLE_FOR_ROOMS:
+                $this->error = Hpe::TOOMANYPEOPLE_FOR_ROOMS_MSG;
+                break;
+            default:
+                $this->error = null;
+                break;
+        }
+    }
 
     private function assignValues(array $data){
-        $this->hotelPriceInputs = $this->data;
+        if(isset($data["checkin"],$data["checkout"],$data["city"],$data["country"],$data["hotel"],$data["people"],$data["rooms"])){
+            $this->country = $data["country"];
+            $this->city = $data["city"];
+            $this->hotel = $data["hotel"];
+            $this->checkin = $data["checkin"];
+            $this->checkout = $data["checkout"];
+            $this->people = $data["people"];
+            $this->rooms = $data["rooms"];
+        }//if(isset($data["checkin"],$data["checkout"],$data["city"],$data["country"],$data["hotel"],$data["people"],$data["rooms"])){
+        else throw new HotelPriceRequestArrayException(Hpe::HOTELREQUESTARRAY_EXC);
     }
 
     /**
@@ -68,6 +90,20 @@ class HotelPrice implements Hpe{
      */
     private function calcPreventive(){
         $this->errno = 0;
+        $hotel_info = H::HOTELS_LIST[$this->country][$this->city][$this->hotel];
+        if(isset($hotel_info)){
+            $max_people = $hotel_info['max_people'];
+            if($this->people > ($this->rooms * $max_people)){
+                //Too many people for the number of rooms booked
+                $this->errno = Hpe::TOOMANYPEOPLE_FOR_ROOMS;
+            }//if($this->people > ($this->rooms * $max_people)){
+            $price = $hotel_info['price'];
+            $days = $this->dateDaysDifference($this->checkin,$this->checkout);
+            $this->fullPrice = $this->rooms * $days * $price;
+        }//if(isset($hotel_info)){
+        else{
+            $this->errno = Hpe::HOTELINFO_NOTFOUND;
+        }
     }
 }
 ?>
--- app/Interfaces/welcome/hotelprice_errors.php
+++ app/Interfaces/welcome/hotelprice_errors.php
@@ -5,6 +5,10 @@ namespace App\Interfaces\Welcome;
 use App\Interfaces\ExceptionsMessages;
 
 interface HotelPriceErrors extends ExceptionsMessages{
+    const HOTELINFO_NOTFOUND = 1;
+    const TOOMANYPEOPLE_FOR_ROOMS = 2; 
 
+    const HOTELINFO_NOTFOUND_MSG = "L'hotel richiesto non è stato trovato";
+    const TOOMANYPEOPLE_FOR_ROOMS_MSG = "Ci sono troppe persone rispetto alle stanze prenotate";
 }
 ?>
