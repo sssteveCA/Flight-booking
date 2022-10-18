e53d15a2504d8c5b78c7bb7262a5839bfa288555
Acer <Acer@DESKTOP-K09QF87>
2022-10-18 22:57:23 +0200

    Added header comments to properties in hotelprice.php

--- app/Classes/welcome/hotelprice.php
+++ app/Classes/welcome/hotelprice.php
@@ -11,21 +11,63 @@ use App\Traits\HotelPriceTrait;
 class HotelPrice implements Hpe{
     use ErrorTrait, MmCommonTrait,HotelPriceTrait;
 
+    /**
+     * The country which the hotel is situated
+     */
+    private string $country;
+    /**
+     * The city which the hotel is situated
+     */
+    private string $city;
+    /**
+     * The hotel name
+     */
+    private string $hotel;
+    /**
+     * The date of the check-in
+     */
+    private string $checkinDate;
+    /**
+     * The date of the check-out
+     */
+    private string $checkoutDate;
+    /**
+     * The number of people that booked rooms in a hotel
+     */
+    private int $people;
+    /**
+     * The number of hotel rooms booked
+     */
+    private int $rooms;
+
     private array $hotelPriceInputs;
 
     private float $fullPrice;
 
     public function __construct(array $data)
     {
-        $this->hotelPriceInputs = $this->data;
+        $this->assignValues($data);
     }
 
+    public function getCountry(){return $this->country;}
+    public function getCity(){return $this->city;}
+    public function getHotel(){return $this->hotel;}
+    public function getCheckinDate(){return $this->checkinDate;}
+    public function getCheckoutDate(){return $this->checkoutDate;}
+    public function getPeople(){return $this->people;}
+    public function getRooms(){return $this->rooms;}
     public function getFullPrice(){return $this->fullPrice;}
     public function getHotelPriceInputs(){return $this->hotelPriceInputs;}
 
+    private function assignValues(array $data){
+        $this->hotelPriceInputs = $this->data;
+    }
+
+    /**
+     * Calculate the prevenive of an hotel booking request
+     */
     private function calcPreventive(){
-
-
+        $this->errno = 0;
     }
 }
 ?>
--- app/Interfaces/exceptions_messages.php
+++ app/Interfaces/exceptions_messages.php
@@ -9,8 +9,9 @@ interface ExceptionsMessages{
 
     const FLIGHTARRAY_EXC = "L' array che contiene i voli prenotati non è formattato correttamente";
     const FLIGHTSDATAMODIFIED_EXC = "I dati del volo da inserire non corrispondono a quelli originalmente forniti";
+    const HOTELREQUESTARRAY_EXC = "L' array che contiene i dati dell'albergo prenotato non è formattato correttamente";
     const INVALIDDATA_EXC = "Uno o più dati passati alla classe FlightPrice non sono validi";
     const SESSION_ID_EXC = "L'id di sessione non è stato trovato";
-    
+
 }
 ?>
