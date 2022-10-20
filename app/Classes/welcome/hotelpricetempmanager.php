e8799d91c84d4a781670b64dad355d896a206983
Acer <Acer@DESKTOP-K09QF87>
2022-10-21 00:02:50 +0200

    Added app/Classes/welcome/hotelpricetempmanager.php

---
+++ app/Classes/welcome/hotelpricetempmanager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\Classes\Welcome;
+
+class HotelPriceTempManager{
+    
+}
+?>
--- app/Traits/common/hotelsearchcontroller_common_trait.php
+++ app/Traits/common/hotelsearchcontroller_common_trait.php
@@ -3,12 +3,18 @@
 namespace App\Traits\Common;
 
 use App\Interfaces\Hotels as H;
+use App\Models\HotelPriceTemp;
 use Illuminate\Http\Request;
 
 /**
  * This trait is used to store common code in HotelSearchController & HotelSearchControllerApi
  */
 trait HotelSearchControllerCommonTrait{
+
+    private static int $random_times = 75;
+    private ?string $session_id;
+
+    public function getSessionId(){return $this->session_id;}
 
     /**
      * Get the countries list that contains hotels
@@ -94,6 +100,35 @@ trait HotelSearchControllerCommonTrait{
         return [];
     }
 
+    /**
+     * Check if user with session id has shortly before done a hotel price preventive search
+     * 
+     * @param string the session id to search
+     * @return bool true if the generate session id exists in the hotelpricetemp table
+     * */
+    private function checkHotelPriceRequests(string $session_id): bool{
+        $hotelTemp = HotelPriceTemp::where('session_id',$session_id)->get();
+        if($hotelTemp->count() > 0) return true;
+        return false;
+    }
 
+    /**
+     * Generate a random session id to identity the user that does the request
+     */
+    private function setSessionId(){
+        $session_id = "";
+        $classname = __CLASS__;
+        $characters = 'aAbBcCdDeEfFGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
+        $length = strlen($characters);
+        $exists = false;
+        do{
+            for($i = 0; $i < $classname::$random_times; $i++){
+                $c = mt_rand(0, $length-1);
+                $session_id .= $characters[$c];
+            } 
+            $exists = $this->checkHotelPriceRequests($session_id);
+        }while($exists);
+        $this->session_id = $session_id.time();
+    }
 }
 ?>
