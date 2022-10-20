0653a2879c886b443cc8b69cc6885b1067baa33d
Acer <Acer@DESKTOP-K09QF87>
2022-10-20 22:47:39 +0200

    Added app/Models/HotelPriceTemp.php

---
+++ app/Models/HotelPriceTemp.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HotelPriceTemp extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hotelpricetemp';
+    public $timestamps = false;
+}
