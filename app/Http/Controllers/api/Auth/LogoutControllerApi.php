f5d31d0c4b58ce10caeb4b0ae2eb0a4c754d5031
Acer <Acer@DESKTOP-K09QF87>
2022-10-22 20:05:25 +0200

    Fixed fatal errors to logout method in LogoutControllerApi.php

--- app/Http/Controllers/api/Auth/LogoutControllerApi.php
+++ app/Http/Controllers/api/Auth/LogoutControllerApi.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Laravel\Passport\RefreshToken;
 use Laravel\Passport\Token;
 
@@ -14,11 +15,9 @@ class LogoutControllerApi extends Controller
      * Revoke user tokens and logout
      */
     public function logout(Request $request){
-        $user = Auth::user()->token();
-        $user->revoke();
-        $tokens = $user->tokens->pluck('id');
-        Token::whereIn('id',$tokens)->update(['revoked' => true]);
-        RefreshToken::whereIn('access_token_id', $tokens)->update(['revoked' => true]);
+        Auth::user()->tokens->each(function($token, $key){
+            $token->delete();
+        });
         return response()->json([
             'done' => true,
             'msg' => 'Non sei più autenticato'
