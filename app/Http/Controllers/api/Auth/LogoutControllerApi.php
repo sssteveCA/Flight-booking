3cd0afdaa3480b67e24011fae729176558d2a6ec
Acer <Acer@DESKTOP-K09QF87>
2022-10-22 19:18:15 +0200

    Added logout method in LogoutControllerApi.php

--- app/Http/Controllers/api/Auth/LogoutControllerApi.php
+++ app/Http/Controllers/api/Auth/LogoutControllerApi.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\api\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\RefreshToken;
+use Laravel\Passport\Token;
 
 class LogoutControllerApi extends Controller
 {
-    //
+    /**
+     * Revoke user tokens and logout
+     */
+    public function logout(Request $request){
+        $user = Auth::user()->token();
+        $user->revoke();
+        $tokens = $user->tokens->pluck('id');
+        Token::whereIn('id',$tokens)->update(['revoked' => true]);
+        RefreshToken::whereIn('access_token_id', $tokens)->update(['revoked' => true]);
+        return 'logged out';
+    }
 }
