b13f73429c1780052aec2d8bfda56a4a961bfb72
Acer <Acer@DESKTOP-K09QF87>
2022-10-22 19:03:18 +0200

    Added app/Http/Controllers/api/Auth/LogoutControllerApi.php

---
+++ app/Http/Controllers/api/Auth/LogoutControllerApi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogoutControllerApi extends Controller
+{
+    //
+}
