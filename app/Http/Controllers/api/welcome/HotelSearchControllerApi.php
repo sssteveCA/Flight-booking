db24f1107b8ec417559f562975431937be60438f
Acer <Acer@DESKTOP-K09QF87>
2022-11-01 20:19:30 +0100

    Added app/Http/Controllers/api/wecome/HotelSearchControllerApi.php

---
+++ app/Http/Controllers/api/welcome/HotelSearchControllerApi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api\welcome;
+
+use App\Http\Controllers\Controller;
+use App\Traits\Common\HotelSearchControllerCommonTrait;
+use Illuminate\Http\Request;
+
+class HotelSearchControllerApi extends Controller
+{
+    use HotelSearchControllerCommonTrait;
+    
+}
