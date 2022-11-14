765cb73895658ea309be852fa08d172b2c542172
Acer <Acer@DESKTOP-K09QF87>
2022-11-14 22:31:11 +0100

    Removed hotelcountries.ts, hotelcitiescountry.ts, hotelscity.ts and hotelinfo.ts from resources/js/classes/hotel folder

--- resources/js/welcome.hotel.ts
+++ resources/js/welcome.hotel.ts
@@ -1,21 +1,6 @@
-import NoArgumentError from "./classes/errors/noargumenterror";
-import HotelCitiesCountry from "./classes/hotel/hotelcitiescountry";
-import HotelCountries from "./classes/hotel/hotelcountries";
-import HotelInfo from "./classes/hotel/hotelinfo";
-import HotelsAvailable from "./classes/hotel/hotelsavailable";
-import HotelsCity from "./classes/hotel/hotelscity";
-import HotelCitiesCountryInterface from "./interfaces/hotel/hotelcitiescountry.interface";
-import HotelCountriesInterface from "./interfaces/hotel/hotelcountries.interface";
-import HotelInfoInterface from "./interfaces/hotel/hotelinfo.interface";
-import HotelsAvailableInterface from "./interfaces/hotel/hotelsavailable.interface";
-import HotelsCityInterface from "./interfaces/hotel/hotelscity.interface";
 
-namespace Globals{
-    export let hc: HotelCountries;
-    export let hcc: HotelCitiesCountry;
-    export let hCity: HotelsCity;
-    export let hi: HotelInfo;
-}
+import HotelsAvailable from "./classes/hotel/hotelsavailable";
+import HotelsAvailableInterface from "./interfaces/hotel/hotelsavailable.interface";
 
 export function loadHotelInfo(): void{
     let ha_data: HotelsAvailableInterface = {
