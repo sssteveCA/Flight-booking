829eb4afd2df6835212876f374ccbe30fbd9b885
Acer <Acer@DESKTOP-K09QF87>
2022-12-05 20:08:17 +0100

    Added hotels table row insertion to create_hotel method in hotelcontroller_common_trait.php

--- app/Interfaces/exceptions_messages.php
+++ app/Interfaces/exceptions_messages.php
@@ -10,6 +10,7 @@ interface ExceptionsMessages{
     const FLIGHTARRAY_EXC = "L' array che contiene i voli prenotati non è formattato correttamente";
     const FLIGHTSDATAMODIFIED_EXC = "I dati del volo da inserire non corrispondono a quelli originalmente forniti";
     const HOTELDATAMODIFIED_EXC = "I dati delLa prenotazione della stanza d'albergo non corrispondono a quelli originalmente forniti";
+    const HOTEL_NEWROW_EXC = "Errore durante l'inserimento di una nuova riga nella tabella hotels";
     const HOTELPT_NEWROW_EXC = "Errore durante l'inserimento di una nuova riga nella tabella hotelpricetemp";
     const HOTELREQUESTARRAY_EXC = "L' array che contiene i dati dell'albergo prenotato non è formattato correttamente";
     const INVALIDDATA_EXC = "Uno o più dati passati alla classe FlightPrice non sono validi";
--- app/Traits/common/hotelcontroller_common_trait.php
+++ app/Traits/common/hotelcontroller_common_trait.php
@@ -2,9 +2,11 @@
 
 namespace App\Traits\Common;
 
+use App\Exceptions\DatabaseInsertionException;
 use App\Exceptions\HotelDataModifiedException;
 use App\Models\HotelPriceTemp;
 use App\Interfaces\ExceptionsMessages as Em;
+use App\Models\Hotel;
 use Illuminate\Support\Facades\Log;
 
 /**
@@ -13,11 +15,29 @@ use Illuminate\Support\Facades\Log;
 trait HotelControllerCommonTrait{
 
 
+    /**
+     * Insert a new hotel room booking in hotels table
+     */
     private function create_hotel(string $session_id){
         $hotelTemp = HotelPriceTemp::where('session_id',$session_id)->first();
         if(!$hotelTemp)
             throw new HotelDataModifiedException(Em::HOTELDATAMODIFIED_EXC);
         Log::channel('stdout')->info("HotelControllerCommonTrait create_hotel hotelTemp => ".var_export($hotelTemp,true));
+        $hotel = new Hotel;
+        $hotel->user_id = auth()->user()->id;
+        $hotel->country = $hotelTemp->country;
+        $hotel->city = $hotelTemp->city;
+        $hotel->hotel = $hotelTemp->hotel;
+        $hotel->booking_date = date("Y-m-d");
+        $hotel->checkin = $hotelTemp->checkin;
+        $hotel->checkout = $hotelTemp->checkout;
+        $hotel->rooms = $hotelTemp->rooms;
+        $hotel->people = $hotelTemp->people;
+        $hotel->price = $hotelTemp->price;
+        $saved = $hotel->save();
+        if(!$saved)
+            throw new DatabaseInsertionException(Em::HOTEL_NEWROW_EXC);
+        $hotelTemp->delete();
     }
 }
 ?>
