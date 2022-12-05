65c423896264ed75cc0eaa9c507b1c6a78d72533
Acer <Acer@DESKTOP-K09QF87>
2022-12-05 21:57:37 +0100

    Added Paypal hidden input data to bookhotel.blade.php

--- resources/views/bookhotel/bookhotel.blade.php
+++ resources/views/bookhotel/bookhotel.blade.php
@@ -16,7 +16,26 @@
             <p class="lead text-center">{{ $message }}</p>
         </div>
         <div class="form-div">
-            <form id="fHotelPrice" method="post" action="">
+            <form id="fHotelPrice" method="post" action="#">
+                <div class="my-3">
+                    <input type="hidden" name="cmd" value="_cart">
+                    <input type="hidden" name="upload" value="1">
+                    <input type="hidden" name="no_note" value="0">
+                    <input type="hidden" name="bn" value="RB_BuyNow_WPS_IT">
+                    <input type="hidden" name="tax_cart" value="0.00">
+                    <input type="hidden" name="rm" value="2">
+                    <input type="hidden" name="business" value="{{ env('PAYPAL_BUSINNESS_EMAIL') }}">
+                    <input type="hidden" name="handling_cart" value="0">
+                    <input type="hidden" name="currency_code" value="EUR">
+                    <input type="hidden" name="lc" value="IT">
+                    <input type="hidden" name="return" value="{{ route(P::ROUTE_PAYPAL_RETURN) }}">
+                    <input type="hidden" name="cbt" value="Torna al sito">
+                    <input type="hidden" name="cancel_return" value="{{ route(P::ROUTE_PAYPAL_CANCEL) }}">
+                </div>
+                <div class="my-3">
+                    <input type="hidden" name="item_name" value="{{ $hotel }}">
+                    <input type="hidden" name="amount" value="{{ $price }}">
+                </div>
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary btn-lg">PAGA</button>
                 </div>
