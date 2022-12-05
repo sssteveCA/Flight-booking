60502f33bfb8d0722894e64ea6ecf1aa7e3d5396
Acer <Acer@DESKTOP-K09QF87>
2022-12-05 19:31:41 +0100

    Added create_hotel method to hotelcontroller_common_trait.php

--- app/Traits/common/hotelcontroller_common_trait.php
+++ app/Traits/common/hotelcontroller_common_trait.php
@@ -2,11 +2,22 @@
 
 namespace App\Traits\Common;
 
+use App\Exceptions\HotelDataModifiedException;
+use App\Models\HotelPriceTemp;
+use App\Interfaces\ExceptionsMessages as Em;
+use Illuminate\Support\Facades\Log;
+
 /**
  * This trait contain common code between HotelController and HotelControllerApi
  */
 trait HotelControllerCommonTrait{
 
 
+    private function create_hotel(string $session_id){
+        $hotelTemp = HotelPriceTemp::where('session_id',$session_id)->first();
+        if(!$hotelTemp)
+            throw new HotelDataModifiedException(Em::HOTELDATAMODIFIED_EXC);
+        Log::channel('stdout')->info("HotelControllerCommonTrait create_hotel hotelTemp => ".var_export($hotelTemp,true));
+    }
 }
 ?>
