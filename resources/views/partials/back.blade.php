81be8e0bfd20ad7a494677fa30124c64ed415dc4
Acer <Acer@DESKTOP-K09QF87>
2022-12-06 09:05:42 +0100

    Added resources/views/back.blade.php

---
+++ resources/views/partials/back.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid py-5">
+    <div class="row justify-content-center">
+        <div id="back-button" class="col-10 d-flex justify-content-center justify-content-md-start">
+            <img src="{{ $back_url }}" alt="Indietro" title="Indietro">
+        </div>
+    </div>
+</div>
