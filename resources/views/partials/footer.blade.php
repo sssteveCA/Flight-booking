c8b848cf44ee7cac34e0591c8a355243611fe05c
stepp <stepp@mail.com>
2022-12-18 17:31:00 +0100

    Updated flight.scss, removed borders from footer rows

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,16 +1,16 @@
 <div class="footer invisible">
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row footer-row justify-content-center">
             <div class="col-12 col-lg-10">
                 Flight Booking è un'applicazione web che ti permette di prenotare voli da più compagnie in pochi passi. E' sufficiente registrarsi gratuitamente alla piattaforma.
             </div>
         </div>
-        <div class="row justify-content-center">
+        <div class="row footer-row justify-content-center">
             <div class="col-12 col-lg-10">
                 Questa applicazione è sviluppata da <a href="https://github.com/sssteveCA/Flight-booking">Stefano Puggioni</a>.
             </div>
         </div>
-        <div class="row justify-content-center">
+        <div class="row footer-row justify-content-center">
             <div class="col-12 col-lg-10">
                 © 2022 - FlightBooking
             </div>
