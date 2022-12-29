17f82dffaf1eb7c158b4ce2cc242119fafc1d3cb
stepp <stepp@mail.com>
2022-12-29 19:14:16 +0100

    Updated return method in PaypalHotelController.php, Added hotel record update operations

--- app/Http/Controllers/paypal/PaypalHotelController.php
+++ app/Http/Controllers/paypal/PaypalHotelController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Interfaces\Constants as C;
 use App\Interfaces\Paths as P;
+use App\Models\Hotel;
+use DateTime;
 use Exception;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Log;
@@ -32,6 +34,15 @@ class PaypalHotelController extends Controller
             Log::channel('stdout')->debug("PaypalHotelController return post => ".var_export($post_data,true));
             if(isset($post_data['payer_status'])){
                 if($post_data['payer_status'] == "VERIFIED"){
+                    $id = $post_data['item_number'];
+                    $hotel = Hotel::find($id);
+                    if($hotel != null){
+                        $hotel->payed = 1;
+                        $datetime = new DateTime($post_data['payment_date']);
+                        $hotel->payed_date = $datetime->format('Y-m-d H:i:s');
+                        $hotel->transaction_id = $post_data['txn_id'];
+                        $hotel->save();
+                    }//if($hotel != null){
                     return response()->view(P::VIEW_HOTEL_PAYPAL_RETURN, [
                         'payment' => 'completed',
                         C::KEY_MESSAGE => C::OK_HOTELPAYMENT
@@ -42,7 +53,7 @@ class PaypalHotelController extends Controller
                     C::KEY_MESSAGE => C::ERR_HOTELPAYMENT_REFUSE
                 ],400);
             }//if(isset($post_data['payer_status'])){
-                throw new Exception("");
+            throw new Exception("");
         }catch(Exception $e){
             throw new HttpResponseException(
                 response()->view(P::VIEW_HOTEL_PAYPAL_RETURN,[
