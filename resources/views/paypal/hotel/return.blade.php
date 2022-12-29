4cbc0bc49bfad8c2270a835325045361dfe04548
stepp <stepp@mail.com>
2022-12-29 18:16:02 +0100

    Added resources/views/paypal/hotel/return.blade.php

---
+++ resources/views/paypal/hotel/return.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.menu')
+
+@section('content')
+    {{-- @isset($post_data)
+        @php
+            echo '<pre>';
+            var_dump($post_data);
+            echo '</pre>';
+        @endphp
+    @endisset
+    --}}
+    <div class="container mt-5">
+            <h2 class="mt-5 text-center">Prenotazione stanza d'albergo</h2>
+            @if(isset($payment) and $payment == 'completed')
+                <p class="lead text-center">{{ $message }}</p>
+            @else
+                <div class="alert alert-danger" role="alert">{{ $message }}</div>
+            @endif
+    </div>
+@endsection
