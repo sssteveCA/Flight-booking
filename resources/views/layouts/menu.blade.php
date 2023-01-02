8cbb7a0914e8cff55ef966569dede7416f29afb0
stepp <stepp@mail.com>
2023-01-02 23:37:07 +0100

    Added 'Le mie stanze' menu item to menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -47,6 +47,7 @@
                                 <div class="dropdown-menu dropdown-menu-right w-100" aria-labelledby="navbarDropdown">
                                     <a href="{{ route(P::ROUTE_INFO) }}" class="dropdown-item">Il mio profilo</a>
                                     <a href="{{ route('myFlights.index') }}" class="dropdown-item">I miei voli</a>
+                                    <a href="{{ route('myHotels.index') }}" class="dropdown-item">Le mie stanze</a>
                                     <a id="logout-item" class="dropdown-item" href="{{ route('logout') }}">
                                         {{ __('Logout') }}
                                     </a>
