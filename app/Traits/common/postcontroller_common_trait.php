518809ac3056365d1c102a0478345be85c16a732
stepp <stepp@mail.com>
2023-01-14 19:41:08 +0100

    Added changeDates method to postcontroller_common_trait.php

--- app/Traits/common/postcontroller_common_trait.php
+++ app/Traits/common/postcontroller_common_trait.php
@@ -4,11 +4,31 @@ namespace App\Traits\Common;
 
 use App\Models\Post;
 use App\Interfaces\Constants as C;
+use App\Traits\DateTrait;
 
 /**
  * This trait contain common code between PostController and PostControllerApi
  */
 trait PostControllerCommonTrait{
+
+    use DateTrait;
+
+    /**
+     * Change the date format of each post in the retrieved collection
+     * @param array $posts_array the array retried from the database
+     * @return array an array with all the dates with the format changed
+     */
+    private function changeDates(array $posts_array): array{
+        $new_posts_array = [];
+        $i = 0;
+        foreach($posts_array as $post){
+            $new_posts_array[$i] = $post;
+            $new_posts_array[$i]['created_at'] = $this->formatDate($post['created_at']);
+            $new_posts_array[$i]['updated_at'] = $this->formatDate($post['updated_at']);
+            $i++;
+        }
+        return $new_posts_array;
+    }
 
     /**
      * Set the response data for PostController index method route
@@ -17,7 +37,7 @@ trait PostControllerCommonTrait{
         $posts = Post::orderBy('updated_at','DESC')->get();
         $n_posts = $posts->count();
         if($n_posts > 0){
-            $posts_array = $posts->toArray();
+            $posts_array = $this->changeDates($posts->toArray());
             return [
                 C::KEY_CODE => 200,
                 C::KEY_RESPONSE => [
