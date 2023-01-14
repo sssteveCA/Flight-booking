f8d954b4beb99b47918f49f62cad64af9b74ff6f
stepp <stepp@mail.com>
2023-01-14 22:07:16 +0100

    Added resources/js/classes/flight/flighteventshtml.ts

---
+++ resources/js/classes/flight/flighteventshtml.ts
@@ -0,0 +1,68 @@
+import HtmlCardInterface from "../../interfaces/htmlcard.interface";
+import { Constants } from "../../values/constants";
+import FlightEvent from "./flightevent";
+
+
+export default class FlightEventsHtml{
+    private _flights_event: Array<FlightEvent> = new Array();
+    private _html: string = "";
+
+    constructor(json: object){
+        this.htmlSet(json);
+    }
+
+    get flights_event(){ return this._flights_event; }
+    get html(){ return this._html; }
+
+
+    private htmlCard(data: HtmlCardInterface):string{
+        let htmlCard = `
+<div class="card col-12 col-sm-6 col-md-4 col-lg-3">
+    <img src="${data.image}" alt="${data.name}" title="${data.name}">
+    <div class="card-body">
+        <h3>${data.name}</h3>
+        <div class="card-text d-flex justify-content-between">
+            <div class="fs-6">${data.location}</div>
+            <div class="fs-6">${data.country}</div>
+        </div>
+        <div class="card-text d-flex justify-content-between">
+            <div class="fs-5">${data.price}</div>
+            <a href="#" class="btn btn-warning">Biglietti</a>
+        </div>
+    </div>
+</div>
+`;
+        return htmlCard;
+    }
+
+    private htmlSet(json: object):void{
+        if(json["done"] == true && json["empty"] == false){
+            this._flights_event = json["list"];
+            let cards = ``;
+            //Add cards elements to result
+            this._flights_event.forEach((val,index)=>{
+                let fel_elem: HtmlCardInterface= {
+                    image: Constants.FOLDER_FLIGHTEVENTS+'/'+val['image'],
+                    name: val['name'],
+                    location: val['location'],
+                    country: val['country'],
+                    price: val['price']
+                };
+                cards += this.htmlCard(fel_elem);
+            });
+            this._html = `
+<div class="container-fluid">
+    <div class="row">
+        ${cards}
+    </div>
+</div>
+`;
+        }//if(json["done"] == true && json["empty"] == false){
+        else{
+            if(json["empty"] == true)
+                this._html = `<div class="alert alert-secondary" role="alert">${json["message"]}</div>`;
+            else
+                this._html = `<div class="alert alert-danger" role="alert">${json["message"]}</div>`;
+        }//else di if(json["done"] == true && json["empty"] == false){  
+    }
+}
--- resources/js/classes/flight/flighteventslist.ts
+++ resources/js/classes/flight/flighteventslist.ts
@@ -3,13 +3,12 @@ import {Constants} from "../../values/constants";
 import HtmlCardInterface from "../../interfaces/htmlcard.interface";
 import FlightEventInterface from "../../interfaces/flight/flightevent.interface";
 import {Config} from "../../../../config";
+import FlightEventsHtml from "./flighteventshtml";
 
 
 export default class FlightEventsList{
-    _flight_events: Array<FlightEvent> = new Array();
-    _html: string;
-    _errno: number = 0;
-    _error: string|null = null;
+    private _errno: number = 0;
+    private _error: string|null = null;
 
     //Numbers
     private static ERR_SCRIPT_EXCEPTION:number = 1;
@@ -22,8 +21,6 @@ export default class FlightEventsList{
     constructor(){
     }
 
-    get flight_events(){return this._flight_events;}
-    get html(){return this._html;}
     get errno(){return this._errno;}
     get error(){
         switch(this._errno){
@@ -52,69 +49,18 @@ export default class FlightEventsList{
             this._errno = FlightEventsList.ERR_SCRIPT_EXCEPTION;
             json = { done: false, empty: false, message: this.error };
         }).finally(()=> {
-            this.htmlSet(json);
+            let feh: FlightEventsHtml = new FlightEventsHtml(json);
         })
         return ok;
     }
 
     private async flight_event_request_promise(): Promise<string>{
-        return new Promise((resolve,reject) => {
+        return await new Promise<string>((resolve,reject) => {
             fetch(FlightEventsList.SCRIPT_URL).then(res => {
                 resolve(res.text());
             }).catch(err =>{
                 reject(err);
             });
         });
-    }
-
-    private htmlSet(json: object):void{
-        if(json["done"] == true && json["empty"] == false){
-            this._flight_events = json["list"];
-            let cards = ``;
-            //Add cards elements to result
-            this._flight_events.forEach((val,index)=>{
-                let fel_elem: HtmlCardInterface= {
-                    image: Constants.FOLDER_FLIGHTEVENTS+'/'+val['image'],
-                    name: val['name'],
-                    location: val['location'],
-                    country: val['country'],
-                    price: val['price']
-                };
-                cards += this.htmlCard(fel_elem);
-            });
-            this._html = `
-<div class="container-fluid">
-    <div class="row">
-        ${cards}
-    </div>
-</div>
-`;
-        }//if(json["done"] == true && json["empty"] == false){
-        else{
-            if(json["empty"] == true)
-                this._html = `<div class="alert alert-secondary" role="alert">${json["message"]}</div>`;
-            else
-                this._html = `<div class="alert alert-danger" role="alert">${json["message"]}</div>`;
-        }//else di if(json["done"] == true && json["empty"] == false){  
-    }
-
-    private htmlCard(data: HtmlCardInterface):string{
-        let htmlCard = `
-<div class="card col-12 col-sm-6 col-md-4 col-lg-3">
-    <img src="${data.image}" alt="${data.name}" title="${data.name}">
-    <div class="card-body">
-        <h3>${data.name}</h3>
-        <div class="card-text d-flex justify-content-between">
-            <div class="fs-6">${data.location}</div>
-            <div class="fs-6">${data.country}</div>
-        </div>
-        <div class="card-text d-flex justify-content-between">
-            <div class="fs-5">${data.price}</div>
-            <a href="#" class="btn btn-warning">Biglietti</a>
-        </div>
-    </div>
-</div>
-`;
-        return htmlCard;
     }
 }
