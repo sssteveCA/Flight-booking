0913d750e87e6ad4bdab08774510eeaf83e7a6ce
stepp <stepp@mail.com>
2023-02-03 23:11:21 +0100

    Added migration for 'user_id' columns dropping of 'flights' and 'hotel' table

--- resources/js/partials/footer.ts
+++ resources/js/partials/footer.ts
@@ -1,6 +1,6 @@
 
 $(()=>{
-    let footer: JQuery = $('.footer');
+    let footer: JQuery<HTMLDivElement> = $('.footer');
     if(footer.length){
         footerPosition(footer);
         $(window).on('resize',()=>{
@@ -15,16 +15,20 @@ $(()=>{
  * @param footerEl 
  */
 function footerPosition(footerEl: JQuery): void{
+    let navbarHeight: number = $('.navbar').height() as number;
+    let contentHeight: number = $('.content').height() as number;
     let windowHeight: number = $(window).height() as number;
     let windowInnerHeight: number = window.innerHeight;
     let bodyHeight: number = $('body').height() as number;
     //let footerPos: JQuery.Coordinates = footerEl.position();
     let footerHeight: number = footerEl.height() as number;
-    /* console.log("footerHeight => "+footerHeight);
+    /* console.log("navbarHeight => "+navbarHeight);
+    console.log("contentHeight => "+contentHeight);
+    console.log("footerHeight => "+footerHeight);
     console.log("bodyHeight => "+bodyHeight);
     console.log("windowInnerHeight => "+windowInnerHeight);
     console.log("windowHeight => "+windowHeight); */
-    if(bodyHeight < (windowInnerHeight - 0.3)){
+    if((navbarHeight+contentHeight) < (windowInnerHeight - 0.3)){
         footerEl.css({
             position: 'fixed',
             bottom: '0px'
