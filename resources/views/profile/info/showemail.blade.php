c7406f3fa8f0040b50642710f9a5e1291ad1abe4
stepp <stepp@mail.com>
2023-02-11 17:38:32 +0100

    Updated showemail.blade.php, added to .email-address div, logged email

--- resources/views/profile/info/showemail.blade.php
+++ resources/views/profile/info/showemail.blade.php
@@ -1,5 +1,5 @@
 <div class="show-email pb-5 mb-5 mt-2 border-bottom border-secondary">
     <span>Indirizzo email</span>
     <div class="divider"></div>
-    <div class="email-address">Il tuo indirizzo email:<b></b></div>
+    <div class="email-address">Il tuo indirizzo email:<b class="ms-2">{{isset($user->email) ? $user->email : 'Sconosciuto'}}</b></div>
 </div>
