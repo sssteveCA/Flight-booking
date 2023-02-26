b24f04f4c733e5b9b34c26e081c09d939f8f62f8
stepp <stepp@mail.com>
2023-02-26 20:06:22 +0100

    Added getCompanyCarFleet and getRentalCarArray methods to carrentalsearchcontroller_common_trait.php

--- app/Interfaces/carrental.php
+++ app/Interfaces/carrental.php
@@ -4,6 +4,10 @@ namespace App\Interfaces;
 
 interface CarRental{
 
+    const AGE_RANGES = [
+        [19,20],[21,24],[25,29],[30,65],[66,75],[76,90]
+    ];
+
     const AVAILABLE_LOCATIONS = [
         'Austria' => [
             'Klaugefurt' => [],
@@ -472,52 +476,72 @@ interface CarRental{
     const CARRENTAL_COMPANIES = [
         'Alamo' => [
             'day_price_30_65' => 11.00,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 15, 1 => 10, 2 => 5, 3 => 0, 4 => 7.5, 5 => 13,
+            ],
             'cars' => [0,3,5,9,12,17,22,23,27,32,34,38,47,49]
         ],
         'Avis' => [
             'day_price_30_65' => 14.34,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 17.5, 1 => 11, 2 => 6.2, 3 => 0, 4 => 9, 5 => 14.5,
+            ],
             'cars' => [4,7,9,12,14,16,19,22,23,28,30,33,38]
         ],
         'Budget' => [
             'day_price_30_65' => 12.21,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 13.9, 1 => 9, 2 => 3.7, 3 => 0, 4 => 5.5, 5 => 10.6,
+            ],
             'cars' => [7,12,17,22,25,26,27,30,33,37,39,42,43,46,48,49,50]
         ],
         'Enterprise' => [
             'day_price_30_65' => 13.39,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 16, 1 => 10.5, 2 => 5, 3 => 0, 4 => 7, 5 => 12,
+            ],
             'cars' => [2,4,9,12,15,19,23,28,38,41,44]
         ],
         'GoldCar' => [
             'day_price_30_65' => 9.95,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 14, 1 => 11.5, 2 => 7, 3 => 0, 4 => 9, 5 => 13.8,
+            ],
             'cars' => [4,9,11,14,17,20,21,22,23,26,29,30,33,36,37,40,52]
         ],
         'Locauto' => [
             'day_price_30_65' => 11.21,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 17.3, 1 => 13.0, 2 => 8.1, 3 => 0, 4 => 9, 5 => 12.4,
+            ],
             'cars' => [2,7,12,15,19,23,27,30,31,33,36,39,40,43,45,46,48]
         ],
         'Maggiore' => [
             'day_price_30_65' => 12.99,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 12.9, 1 => 9.2, 2 => 3, 3 => 0, 4 => 6.2, 5 => 10.5,
+            ],
             'cars' => [1,4,5,9,12,13,16,17,20,21,22,26,29,30,32,34,37,40,41] 
         ],
         'National' => [
             'day_price_30_65' => 14.00,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 16.4, 1 => 10.9, 2 => 6.7, 3 => 0, 4 => 8.5, 5 => 14,
+            ],
             'cars' => [0,2,6,9,10,14,16,19,22,23,26,28,30,31,37,45,46,47,50]
         ],
         'Sixt' => [
             'day_price_30_65' => 12.42,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 13.9, 1 => 11.1, 2 => 5.5, 3 => 0, 4 => 10, 5 => 14.2,
+            ],
             'cars' => [4,6,9,10,13,15,17,20,21,22,24,26,29,32,35,36,39,41,43,47,50,51,52]
         ],
         'TopCar' => [
             'day_price_30_65' => 15.59,
-            'day_price_additional_pcg' => 15,
+            'day_price_additional_pcg' => [
+                0 => 17.4, 1 => 12, 2 => 8, 3 => 0, 4 => 7, 5 => 11.9,
+            ],
             'cars' => [2,3,8,11,14,17,21,24,26,27,29,32,33,38,41,46,49,50]
         ],
     ];
--- app/Traits/common/carrentalsearchcontroller_common_trait.php
+++ app/Traits/common/carrentalsearchcontroller_common_trait.php
@@ -2,10 +2,42 @@
 
 namespace App\Traits\Common;
 
+use App\Interfaces\CarRental as Cr;
+
 /**
  * This trait contains same code for CarRentalSearchController & CarRentalSearchControllerApi
  */
 trait CarRentalSearchControllerCommonTrait{
 
+    /**
+     * Add the cars info to each company
+     * @param $company_data the array with the company info to update
+     * @return array
+     */
+    private function getCompanyCarFleet(array $company_data): array{
+        $cars_array = [];
+        foreach($company_data['cars'] as $num_car){
+            $cars_array[] = Cr::CAR_FLEET[$num_car];
+        }
+        return $cars_array;
+    }
+
+    /**
+     * Set the car rental information for the response
+     * @return array
+     */
+    private function getRentalCarData(): array{
+        $companies_data = Cr::CARRENTAL_COMPANIES;
+        foreach($companies_data as $key => $company_data){
+            $companies_data[$key]['cars'] = $this->getCompanyCarFleet($company_data);
+        }
+        return [
+            'age_ranges' => Cr::AGE_RANGES,
+            'available_locations' => Cr::AVAILABLE_LOCATIONS,
+            'companies_data' => $companies_data
+        ];
+    }
+
+    
 }
 ?>
