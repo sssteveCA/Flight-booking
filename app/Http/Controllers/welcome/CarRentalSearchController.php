53e8b3527f31f4d111838cbf30568edc83ee73c8
stepp <stepp@mail.com>
2023-02-26 19:27:38 +0100

    Added app/Http/Controllers/welcome/CarRentalSearchController.php

---
+++ app/Http/Controllers/welcome/CarRentalSearchController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\welcome;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CarRentalSearchController extends Controller
+{
+    //
+}
