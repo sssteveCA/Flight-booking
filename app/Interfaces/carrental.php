34fb7c0084af1ad38c4c5497b258a14419b8ca52
stepp <stepp@mail.com>
2023-02-26 19:17:03 +0100

    Updated form in carrental.blade.php

--- app/Interfaces/carrental.php
+++ app/Interfaces/carrental.php
@@ -309,216 +309,216 @@ interface CarRental{
 
     const CAR_FLEET = [
         'Alfa Romeo Stelvio' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.30, 'images' => []
         ],
         'Bmw Serie 2' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 14.32, 'images' => []
         ],
         'Cadillac XT4' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 9.95, 'images' => []
         ],
         'Citroen C1' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.04, 'images' => []
         ],
         'Citroen C4 Spacetourer' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.56, 'images' => []
         ],
         'Citroen Grand C4 Spacetourer' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.05, 'images' => []
         ],
         'Citroen SpaceTourer 9 Pax' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 9.85, 'images' => []
         ],
         'Cupra Formentor' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 15.00, 'images' => []
         ],
         'Fiat 500' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.99, 'images' => []
         ],
         'Fiat 500 Cabrio, Cambio Automatico' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.43, 'images' => []
         ],
         'Fiat 500 Cabrio, Cambio Manuale' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 10.50, 'images' => []
         ],
         'Fiat Panda' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 10.25, 'images' => []
         ],
         'Ford C-Max' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.60, 'images' => []
         ],
         'Ford Ecosport' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.33, 'images' => []
         ],
         'Ford Fiesta' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 14.00, 'images' => []
         ],
         'Ford Focus' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 15.29, 'images' => []
         ],
         'Ford Focus 1.5 TDC' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 14.04, 'images' => []
         ],
         'Ford Focus SW' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.58, 'images' => []
         ],
         'Ford Galaxy 7 posti' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 9.99, 'images' => []
         ],
         'Ford Mondeo SW' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.15, 'images' => []
         ],
         'Ford Mustang Mach-E' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.99, 'images' => []
         ],
         'Ford Tourneo Connect' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.50, 'images' => []
         ],
         'Ford Tourneo 9 posti L1H1' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 10.67, 'images' => []
         ],
         'Ford Transit 9 posti L2H1' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 16.08, 'images' => []
         ],
         'Hyundai Tucson' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.99, 'images' => []
         ],
         'Lexus ES Hybrid' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 9.92, 'images' => []
         ],
         'Lexus NX' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 10.35, 'images' => []
         ],
         'Lynk & Co 01' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.00, 'images' => []
         ],
         'Mahindra Kuv100' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.66, 'images' => []
         ],
         'Maserati Ghibli' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.07, 'images' => []
         ],
         'Mercedes Amg GT Coupe' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.50, 'images' => []
         ],
         'Mercedes Classe A' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.75, 'images' => []
         ],
         'Mercedes Classe B' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.90, 'images' => []
         ],
         'Mercedes Classe C Cabrio' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 14.00, 'images' => []
         ],
         'Mercedes Classe C Station Wagon' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 14.56, 'images' => []
         ],
         'Opel Astra Sports Tourer' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 15.88, 'images' => []
         ],
         'Opel Corsa' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.02, 'images' => []
         ],
         'Opel Mokka' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.79, 'images' => []
         ],
         'Peugeot 108' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 9.75, 'images' => []
         ],
         'Peugeot 308' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.27, 'images' => []
         ],
         'Peugeot 508' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.12, 'images' => []
         ],
         'Renault Clio' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.26, 'images' => []
         ],
         'Renault Talisman Wagon' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.69, 'images' => []
         ],
         'Skoda Kamiq' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 16.12, 'images' => []
         ],
         'Skoda Karoq' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 15.64, 'images' => []
         ],
         'Toyota C-HR' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.89, 'images' => []
         ],
         'Toyota Auris' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.70, 'images' => []
         ],
         'Toyota Aygo' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 11.56, 'images' => []
         ],
         'Toyota Corlla' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 13.00, 'images' => []
         ],
         'Toyota Mirai' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 16.00, 'images' => []
         ],
         'Toyota Proace Electric Aut. 9 Posti' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 15.20, 'images' => []
         ],
         'Toyota Yaris' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 14.56, 'images' => []
         ],
         'Volkswagen Golf' => [
-            'day_price' => 0.00, 'images' => []
+            'day_price' => 12.51, 'images' => []
         ]
     ];
 
     const CARRENTAL_COMPANIES = [
         'Alamo' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 11.00,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [0,3,5,9,12,17,22,23,27,32,34,38,47,49]
         ],
         'Avis' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 14.34,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [4,7,9,12,14,16,19,22,23,28,30,33,38]
         ],
         'Budget' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 12.21,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [7,12,17,22,25,26,27,30,33,37,39,42,43,46,48,49,50]
         ],
         'Enterprise' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 13.39,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [2,4,9,12,15,19,23,28,38,41,44]
         ],
         'GoldCar' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 9.95,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [4,9,11,14,17,20,21,22,23,26,29,30,33,36,37,40,52]
         ],
         'Locauto' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 11.21,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [2,7,12,15,19,23,27,30,31,33,36,39,40,43,45,46,48]
         ],
         'Maggiore' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 12.99,
             'day_price_additional_pcg' => 15,
-            'cars' => [] 
+            'cars' => [1,4,5,9,12,13,16,17,20,21,22,26,29,30,32,34,37,40,41] 
         ],
         'National' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 14.00,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [0,2,6,9,10,14,16,19,22,23,26,28,30,31,37,45,46,47,50]
         ],
         'Sixt' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 12.42,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [4,6,9,10,13,15,17,20,21,22,24,26,29,32,35,36,39,41,43,47,50,51,52]
         ],
         'TopCar' => [
-            'day_price_30_65' => 0.00,
+            'day_price_30_65' => 15.59,
             'day_price_additional_pcg' => 15,
-            'cars' => []
+            'cars' => [2,3,8,11,14,17,21,24,26,27,29,32,33,38,41,46,49,50]
         ],
     ];
 }
--- resources/views/partials/welcome/carrental.blade.php
+++ resources/views/partials/welcome/carrental.blade.php
@@ -2,22 +2,47 @@
     <form id="fCarRental" method="post" action="#">
         <div class="container">
             <div class="row">
-                <div id="fb-rc1" class="col-12">
-                    <input type="text" id="fb-rentcarloc" class="form-control-lg" name="rentcarloc" placeholder="Città o aereoporto per affitare l'auto">
+                <div id="fb-rc1" class="form-floating col-12 col-md-6 col-lg-5">
+                    <select id="fb-rc-rent-company" class="form-select rent-company" name="rent_company"></select>
+                    <label for="fb-rc-rent-company">Compagnia di noleggio</label>
+                </div>
+                <div id="fb-rc2" class="form-floating col-12 col-md-6 col-lg-5">
+                    <select id="fb-rc-car" class="form-select car" name="car"></select>
+                    <label for="fb-rc-car">Modello automobile</label>
                 </div>
             </div>
             <div class="row">
-                <div id="fb-rc2" class="form-floating col-12 col-md-6 col-lg-5">
+                <div id="fb-rc3" class="form-floating col-12 col-md-6 col-lg-5">
+                    <select id="fb-rc-pickup-location" class="form-select pickup-location" name="pickup_location"></select>
+                    <label for="fb-rc-pickup-location">Località di ritiro</label>
+                </div>
+                <div id="fb-rc4" class="form-floating col-12 col-md-6 col-lg-5">
+                    <select id="fb-rc-delivery-location" class="form-select delivery-location" name="delivery_location"></select>
+                    <label for="fb-rc-delivery-location">Località di consegna</label>
+                </div>
+            </div>
+            <div class="row">
+                <div id="fb-rc5" class="form-floating col-12 col-md-6 col-lg-5">
                     <input type="datetime-local" id="fb-rentstart" class="form-control" name="rentstart" >
                     <label for="fb-rentstart">Data di ritiro</label>
                 </div>
-                <div id="fb-rc3" class="form-floating col-12 col-md-6 col-lg-5">
+                <div id="fb-rc6" class="form-floating col-12 col-md-6 col-lg-5">
                     <input type="datetime-local" id="fb-rentend" class="form-control" name="rentend">
                     <label for="fb-rentend">Data di consegna</label>
                 </div>
-                <div id="fb-rc4" class="col-12 col-lg-2">
+            </div>
+            <div class="row">
+                <div id="fb-rc7" class="form-floating col-12 col-md-6 col-lg-5">
+                    <select id="fb-rc-age-range" class="form-select age-range" name="age_range"></select>
+                    <label for="fb-rc-age-range">Fascia d'età</label>
+                </div>
+            </div>
+            <div class="row">
+                <div id="fb-rc8" class="col-12 col-lg-2">
                     <button type="submit" class="btn btn-danger">Andiamo</button>
                 </div>
+            </div>
+                
             </div>
         </div>
     </form>  
