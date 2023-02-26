06b5643bb39678321592e6077c587ddfe9e483e4
stepp <stepp@mail.com>
2023-02-26 22:52:43 +0100

    Added resources/js/interfaces/carrental/carrental.interface.ts

---
+++ resources/js/interfaces/carrental/carrentalevents.interface.ts
@@ -0,0 +1,10 @@
+
+export default interface CarRentalEventsInterface{
+    carrental_company_id: string;
+    car_model_id: string;
+    pickup_location_id: string;
+    delivery_location_id: string;
+    rent_start_id: string;
+    rent_end_id: string;
+    age_range_id: string;
+}
