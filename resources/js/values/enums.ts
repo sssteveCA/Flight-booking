257ecb000e169d6f1deb247dd9c616615ad6936b
stepp <stepp@mail.com>
2023-03-20 21:31:53 +0100

    Added resources/js/values/enums.ts

---
+++ resources/js/values/enums.ts
@@ -0,0 +1,18 @@
+
+/**
+ * The Auto possibile transmissions
+ */
+export enum AutoChangeTypes{
+    AUTOMATIC = 'Automatico',
+    MANUAL = 'Manuale'
+}
+
+/**
+ * The Auto possible power supplies
+ */
+export enum AutoPowerSupplies{
+    DIESEL = 'Diesel',
+    ELECTRIC = 'Elettrica',
+    GASOLINE = 'Benzina',
+    HYBRID = 'Ibrida'
+}
