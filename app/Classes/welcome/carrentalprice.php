3580756c61894b7a2a9902dafb5148c5160efc2a
stepp <stepp@mail.com>
2023-03-21 20:06:58 +0100

    Updated carrentalpriceresult.blade.php

--- app/Classes/welcome/carrentalprice.php
+++ app/Classes/welcome/carrentalprice.php
@@ -41,28 +41,6 @@ class CarRentalPrice{
     public function getRentEndDate(){ return $this->rentend_date; }
     public function getTotalPrice(){ return $this->total_price; }
 
-    private function calcPrice(){
-        $age_ranges = $this->data['age_ranges'];
-        Log::channel('stdout')->info("CarRentalPrice calcPrice age_ranges => ".var_export($age_ranges,true));
-        $age_range_arr = explode("-", $this->age_range);
-        $ar_key = array_search($age_range_arr,$age_ranges);
-        Log::channel('stdout')->info("CarRentalPrice calcPrice ar_key => ".var_export($ar_key,true));
-        $company_arr = $this->data['companies_data'][$this->company_name];
-        Log::channel('stdout')->info("CarRentalPrice calcPrice company_arr => ".var_export($company_arr,true));
-        $car_arr = $this->data['companies_data'][$this->company_name]['cars'][$this->car_name];
-        Log::channel('stdout')->info("CarRentalPrice calcPrice car_arr => ".var_export($car_arr,true));
-        $price_company_part = $company_arr['day_price_30_65'] + ($company_arr['day_price_30_65'] * ($company_arr['day_price_additional_pcg'][$ar_key] / 100));
-        Log::channel('stdout')->info("CarRentalPrice calcPrice price_company_part => ".var_export($price_company_part,true));
-        $price_car_part = $car_arr['day_price'] + ($car_arr['day_price'] * $company_arr['day_price_additional_pcg'][$ar_key] / 100);
-        Log::channel('stdout')->info("CarRentalPrice calcPrice price_car_part => ".var_export($price_car_part,true));
-        $dti_rentstart = DateTimeImmutable::createFromFormat('Y-m-d',$this->rentstart_date);
-        $dti_rentend = DateTimeImmutable::createFromFormat('Y-m-d',$this->rentend_date);
-        $dti_diff = $dti_rentend->diff($dti_rentstart);
-        Log::channel('stdout')->info("CarRentalPrice calcPrice dti_diff => ".var_export($dti_diff,true));
-        $this->total_price = ($price_company_part + $price_car_part) * (float)$dti_diff->d;
-        Log::channel('stdout')->info("CarRentalPrice calcPrice total_price => ".var_export($this->total_price,true));
-    }
-
 }
 
 ?>
--- resources/views/welcome/carrentalpriceresult.blade.php
+++ resources/views/welcome/carrentalpriceresult.blade.php
@@ -1,12 +1,65 @@
 @extends('layouts.page')
 
+@section('namespaces')
+    @php
+        use App\Interfaces\Paths as P;
+    @endphp
+@endsection
+
+@section('title','Prezzo auto noleggio scelta')
+
+@section('links')
+    @parent
+@endsection
+
+@section('scripts')
+    @parent
+@endsection
+
 @section('content')
+    @include(P::VIEW_BACKBUTTON,[
+        'back_image' => '/img/back.png',
+        'back_url' => '../'
+    ])
     @if($done == true)
-        @php
-            echo '<pre>';
-            var_dump($data);
-            echo '</pre>';
-        @endphp
+    <form id="fHotelPrice" method="post" action="#">
+        @csrf
+        @method('POST')
+        <input type="hidden" name="session_id" value="">
+        <div class="carrental-div">
+            <div class="carrental-header bg-success bg-gradient">Preventivo noleggio auto</div>
+            <div class="carrental-row">
+                <div class="column-elem">
+                    <p class="cr-header bg-warning bg-gradient">Nome compagnia</p>
+                    <p class="bg-light bg-gradient">{{ $data['company_name'] }}</p>
+                </div>
+                <div class="column-elem">
+                    <p class="cr-header bg-warning bg-gradient">Modello auto</p>
+                    <p class="bg-light bg-gradient">{{ $data['car_name'] }}</p>
+                </div>
+                <div class="column-elem">
+                    <p class="cr-header bg-warning bg-gradient">La tua fascia d'età</p>
+                    <p class="bg-light bg-gradient">{{ $data['age_range'] }} anni</p>
+                </div>
+                <div class="column-elem">
+                    <p class="cr-header bg-warning bg-gradient">Data di ritiro</p>
+                    <p class="bg-light bg-gradient">{{ $data['rentstart_date'] }}</p>
+                </div>
+                <div class="column-elem">
+                    <p class="cr-header bg-warning bg-gradient">Data di consegna</p>
+                    <p class="bg-light bg-gradient">{{ $data['rentend_date'] }}</p>
+                </div>
+                <div class="column-elem">
+                    <p class="cr-header bg-warning bg-gradient">Prezzo</p>
+                    <p class="bg-light bg-gradient">{{ $data['total_price'] }}€</p>
+                </div>
+            </div>
+        </div>
+        <div class="form-button my-4 text-center">
+            <button type="submit" class="btn btn-primary btn-lg">PRENOTA</button>
+        </div>
+    </form>
+    
     @elseif($done == false)
         @isset($message)
             <x-alert classes="alert alert-danger" :message="$message" />
@@ -19,4 +72,9 @@
             @endforeach
         @endforeach
     @endisset
+    {{--   @php
+            echo '<pre>';
+            var_dump($data);
+            echo '</pre>';
+    @endphp --}}
 @endsection
