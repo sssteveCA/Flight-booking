a16ce5b61eaaa9d6141b0bbea46e05bc889d634f
stepp <stepp@mail.com>
2023-03-21 17:55:49 +0100

    Added failedValidation method to CarRentalPriceRequest.php

--- app/Http/Requests/welcome/CarRentalPriceRequest.php
+++ app/Http/Requests/welcome/CarRentalPriceRequest.php
@@ -4,12 +4,27 @@ namespace App\Http\Requests\welcome;
 
 use App\Traits\Common\CarRentalPriceRequestCommonTrait;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+use App\Interfaces\Constants as C;
+use App\Interfaces\Paths as P;
 
 class CarRentalPriceRequest extends FormRequest
 {
     use CarRentalPriceRequestCommonTrait;
 
     protected $stopOnFirstFailure = true;
+
+    protected function failedValidation(Validator $validator ){
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(
+            response()->view(P::VIEW_CARRENTALPRICERESULT,[
+               C::KEY_DONE => false, 'errors' => $errors
+            ], 400)
+        );
+        
+    }
 
     
 }
