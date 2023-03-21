69e3099b077342d5cf951d631bd9cc5e49063e75
stepp <stepp@mail.com>
2023-03-21 18:24:56 +0100

    Added app/Traits/carrentalprice_trait.php

--- app/Classes/welcome/carrentalprice.php
+++ app/Classes/welcome/carrentalprice.php
@@ -1,14 +1,29 @@
 <?php
 
 namespace App\Classes;
+
+use App\Traits\CarRentalPriceTrait;
 use App\Traits\ErrorTrait;
 use App\Traits\MmCommonTrait;
 
 class CarRentalPrice{
 
-    use ErrorTrait, MmCommonTrait;
+    private string $car_name;
+    private string $company_name;
+    private string $age_range;
 
-    
+    use ErrorTrait, MmCommonTrait, CarRentalPriceTrait;
+
+    public function __construct(array $data){
+        $this->car_name = $data['car_name'];
+        $this->company_name = $data['company_name'];
+        $this->age_range = $data['age_range'];
+    }
+
+    public function getCarName(){ return $this->car_name; }
+    public function getCompanyName(){ return $this->company_name; }
+    public function getAgeRange(){ return $this->age_range; }
+
 }
 
 ?>
