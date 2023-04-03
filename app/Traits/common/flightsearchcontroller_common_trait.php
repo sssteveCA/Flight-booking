1eb448ee49867206230abaaaa33fac0c9444cc0f
stepp <stepp@mail.com>
2023-04-03 12:25:00 +0200

    Added onewayFlight and roundtripFlights methods to flightsearchcontroller_common_trait.php

--- app/Traits/common/flightsearchcontroller_common_trait.php
+++ app/Traits/common/flightsearchcontroller_common_trait.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits\Common;
 
+use App\Classes\Welcome\FlightPrice;
 use App\Interfaces\Constants as C;
 use App\Classes\Welcome\FlightsTempManager;
 use App\Interfaces\Airports as A;
@@ -94,6 +95,72 @@ trait FlightSearchCommonTrait{
         ];
         //Log::channel('stdout')->info('setFlightPrice method data => '.var_export($data,true));
         return $data;
+    }
+
+    /**
+     * Get the array with the oneway flight information
+     * @param FlightPrice $fl_oneway the object with the oneway information
+     * @return array
+     */
+    private function onewayFlight(FlightPrice $fl_oneway): array{
+        return [
+            'oneway' => [
+                'company_name' => $fl_oneway->company_name,
+                'departure_country' => $fl_oneway->departure_country,
+                'departure_airport' => $fl_oneway->departure_airport,
+                'booking_date' => date('Y-m-d'),
+                'flight_date' => $fl_oneway->flight_date,
+                'flight_time' => $fl_oneway->flight_time,
+                'arrival_country' => $fl_oneway->arrival_country,
+                'arrival_airport' => $fl_oneway->arrival_airport,
+                'adults' => $fl_oneway->adults,
+                'teenagers' => $fl_oneway->teenagers,
+                'children' => $fl_oneway->children,
+                'newborns' => $fl_oneway->newborns,
+                'flight_price' => $fl_oneway->flight_price_format
+            ]
+        ];
+    }
+
+    /**
+     * Get the array with the roundtrip flights information
+     * @param FlightPrice $fl_outbound the object with the outbound flight information
+     * @param FlightPrice $fl_return the object with the return flight information
+     * @return array
+     */
+    private function roundtripFlights(FlightPrice $fl_outbound, FlightPrice $fl_return): array{
+        return [
+            'outbound' => [
+                'company_name' => $fl_outbound->company_name,
+                'departure_country' => $fl_outbound->departure_country,
+                'departure_airport' => $fl_outbound->departure_airport,
+                'booking_date' => date('Y-m-d'),
+                'flight_date' => $fl_outbound->flight_date,
+                'flight_time' => $fl_outbound->flight_time,
+                'arrival_country' => $fl_outbound->arrival_country,
+                'arrival_airport' => $fl_outbound->arrival_airport,
+                'adults' => $fl_outbound->adults,
+                'teenagers' => $fl_outbound->teenagers,
+                'children' => $fl_outbound->children,
+                'newborns' => $fl_outbound->newborns,
+                'flight_price' => $fl_outbound->flight_price_format
+            ],
+            'return' => [
+                'company_name' => $fl_return->company_name,
+                'departure_country' => $fl_return->departure_country,
+                'departure_airport' => $fl_return->departure_airport,
+                'booking_date' => date('Y-m-d'),
+                'flight_date' => $fl_return->flight_date,
+                'flight_time' => $fl_return->flight_time,
+                'arrival_country' => $fl_return->arrival_country,
+                'arrival_airport' => $fl_return->arrival_airport,
+                'adults' => $fl_return->adults,
+                'teenagers' => $fl_return->teenagers,
+                'children' => $fl_return->children,
+                'newborns' => $fl_return->newborns,
+                'flight_price' => $fl_return->flight_price_format
+            ]
+        ];
     }
 
     /**
