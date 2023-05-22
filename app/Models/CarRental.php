8dca62d70f90a9f1f3a15a7fd65226044453ce29
stepp <stepp@mail.com>
2023-05-22 22:23:02 +0200

    Added app/Models/CarRental.php

---
+++ app/Models/CarRental.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarRental extends Model
+{
+    use HasFactory;
+
+    protected $table = 'carrentals';
+    public $timestamps = false;
+}
