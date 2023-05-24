bd52364b76d5f16c376d05fac9c812af77fbaecf
stepp <stepp@mail.com>
2023-05-24 10:38:14 +0200

    Added resources/views/paypal/carrental/return.blade.php

---
+++ resources/views/paypal/carrental/return.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.page')
+
+@section('content')
+    <div class="container mt-5">
+        <h2 class="mt-5 text-center">Prenotazione auto</h2>
+        @if(isset($payment) and $payment == 'completed')
+            <p class="lead text-center">{{ $message }}</p>
+        @else
+            <x-alert classes="alert alert-danger" :message="$message" />
+        @endif
+    </div>
+@endsection
