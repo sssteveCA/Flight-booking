72d1e2ffb501223f51f459a9d3d77004cb216644
stepp <stepp@mail.com>
2023-05-24 18:03:56 +0200

    Added getCarRentalPrice method to CarRentalSearchControllerApi.php

--- app/Http/Controllers/api/welcome/CarRentalSearchControllerApi.php
+++ app/Http/Controllers/api/welcome/CarRentalSearchControllerApi.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\api\welcome;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\api\welcome\CarRentalPriceRequestApi;
 use App\Traits\Common\CarRentalSearchControllerCommonTrait;
+use Exception;
 use Illuminate\Http\Request;
+use App\Interfaces\Constants as C;
 
 class CarRentalSearchControllerApi extends Controller
 {
@@ -15,5 +18,20 @@ class CarRentalSearchControllerApi extends Controller
      */
     public function getCarRentalData(){
         return response()->json($this->getRentalCarArray(),200,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE); 
+    }
+
+    /**
+     * Get the preventive for the selected car with the input data
+     */
+    public function getCarRentalPrice(CarRentalPriceRequestApi $request){
+        $data = $request->validated();
+        try{
+            $carrentaldata = $this->getCarRentalPriceInfo($data);
+            return response()->json($carrentaldata,200,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }catch(Exception $e){
+            return response()->json([
+                C::KEY_DONE => false, C::KEY_MESSAGE => C::ERR_CARRENTAL_PREVENTIVE
+            ],500,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }
     }
 }
--- app/Http/Controllers/api/welcome/HotelSearchControllerApi.php
+++ app/Http/Controllers/api/welcome/HotelSearchControllerApi.php
@@ -24,10 +24,9 @@ class HotelSearchControllerApi extends Controller
             $hotelPriceData = $this->getHotelPriceInfo($inputs);
             return response()->json($hotelPriceData["response_array"],$hotelPriceData["response_code"],[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
         }catch(Exception $e){
-            $response_code = 500;
             return response()->json([
                 C::KEY_DONE => false, C::KEY_STATUS => 'ERROR', C::KEY_MESSAGE => C::ERR_HOTEL_PREVENTIVE
-            ],$response_code,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+            ],500,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
         }
     }
 
