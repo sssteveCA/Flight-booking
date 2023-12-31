b4dd7c7f59d205421cd6091d27f44c777ea76233
= <=>
2023-12-31 17:44:11 +0000

    Added FlightFactory definition

--- database/factories/FlightFactory.php
+++ database/factories/FlightFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Interfaces\Airports;
+use Carbon\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flight>
@@ -16,8 +19,33 @@ class FlightFactory extends Factory
      */
     public function definition()
     {
+        $countriesList = array_keys(Airports::AIRPORTS_LIST);
+        $departureCountry =  $this->faker->randomElement($countriesList);
+        $departureAirportsList = array_keys(Airports::AIRPORTS_LIST[$departureCountry]);
+        $departureAirport = $this->faker->randomElement($departureAirportsList);
+        $arrivalCountry = $this->faker->randomElement($countriesList);
+        $arrivalAirportsList = array_keys(Airports::AIRPORTS_LIST[$arrivalCountry]);
+        while(!isset($arrivalAirport) || ($departureCountry == $arrivalCountry && $arrivalAirport == $departureAirport)){
+            $arrivalAirport = $this->faker->randomElement($arrivalAirportsList);
+        }
+        $flightDate = Carbon::tomorrow();
+
         return [
-            //
+            'user_id' => User::factory(),
+            'company_name' => $this->faker->randomElement(Airports::COMPANIES_LIST),
+            'departure_country' => $departureCountry,
+            'departure_airport' => $departureAirport,
+            'arrival_country' => $arrivalCountry,
+            'arrival_airport' => $arrivalAirport,
+            'adults' => $this->faker->randomElement(range(1,5)),
+            'teenagers' => $this->faker->randomElement(range(1,5)),
+            'children' => $this->faker->randomElement(range(1,5)),
+            'newborns' => $this->faker->randomElement(range(1,5)),
+            'flight_date' => $flightDate->format('Y-m-d'),
+            'flight_time' => $this->faker->time(),
+            'flight_price' => $this->faker->randomFloat(2,50,500),
+            'payed' => $this->faker->randomElement([0, 1]),
+            'transaction_id' => $this->faker->uuid,
         ];
     }
 }
