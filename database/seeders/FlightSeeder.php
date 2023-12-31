da73af55f2fd9726fd61b1512b6ea7e4d9f428c6
= <=>
2023-12-31 17:45:48 +0000

    Added FlightSeeder run

--- database/seeders/FlightSeeder.php
+++ database/seeders/FlightSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Flight;
 
 class FlightSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class FlightSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Flight::factory(20)->create();
     }
 }
