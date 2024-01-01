09ccd908a539b4f8d942435648c3573a468414b5
= <=>
2024-01-01 13:27:50 +0000

    Added HotelSeeder run

--- database/seeders/HotelSeeder.php
+++ database/seeders/HotelSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Hotel;
 
 class HotelSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class HotelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Hotel::factory(10)->create();
     }
 }
