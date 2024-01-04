5858895556aabc2600fa3b0bd1f58b80b3a40420
= <=>
2024-01-04 06:51:21 +0000

    Added flighteventbooksearchcontroller_common_trait.php

--- app/Http/Controllers/FlightEventBookSearchController.php
+++ app/Http/Controllers/FlightEventBookSearchController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits\Common;
 
 class FlightEventBookSearchController extends Controller
 {
-    //
+    use FlightEventBookSearchControllerCommonTrait;
+    
+    public function getFlightEventBookPrice(Request $request){
+
+    }
 }
---
+++ app/Traits/common/flighteventbooksearchcontroller_common_trait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Traits\Common;
+
+/**
+ * Trait used by FlightEventBookSearchController web and api classes
+ */
+trait FlightEventBookSearchControllerCommonTrait{
+
+}
+
+?>
