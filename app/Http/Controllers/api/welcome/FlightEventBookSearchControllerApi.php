23e1b91a755321c9c2edcf6a2be6d8cbe1a3e521
= <=>
2024-01-08 22:57:16 +0000

    Added FlightEventPriceRequestApi

--- app/Http/Controllers/api/welcome/FlightEventBookSearchControllerApi.php
+++ app/Http/Controllers/api/welcome/FlightEventBookSearchControllerApi.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\api\welcome;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Traits\Common\FlightEventBookSearchControllerCommonTrait;
+use Exception;
+use App\Interfaces\Constants as C;
+use App\Interfaces\Paths as P;
 
 class FlightEventBookSearchControllerApi extends Controller
 {
-    //
+    use FlightEventBookSearchControllerCommonTrait;
+
 }
