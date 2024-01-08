65d5a4b7fc46158e9cfa51c011ec97a83300fee1
= <=>
2024-01-08 00:14:06 +0000

    Filled FlightEventBookSearchControllerCommonTrait getFlightEventBookPriceInfo

--- app/Traits/common/flighteventbooksearchcontroller_common_trait.php
+++ app/Traits/common/flighteventbooksearchcontroller_common_trait.php
@@ -2,6 +2,10 @@
 
 namespace App\Traits\Common;
 
+use App\Classes\Welcome\FlightEventBookPrice;
+use App\Classes\FlightEventBookTempManager;
+use App\Interfaces\Constants as C;
+
 /**
  * Trait used by FlightEventBookSearchController web and api classes
  */
@@ -13,7 +17,35 @@ trait FlightEventBookSearchControllerCommonTrait{
      * @return array
      */
     private function getFlightEventBookPriceInfo(array $inputs): array{
-        
+        $flightEventBookPrice = new FlightEventBookPrice([
+            'event_id' => $inputs['event_id'],
+            'tickets' => $inputs['tickets']
+        ]);
+        $flightEventBookTempData = [
+            'event_id' => $flightEventBookPrice->getEventId(),
+            'tickets' => $flightEventBookPrice->getTickets(),
+            'price' => $flightEventBookPrice->getPrice()
+        ];
+        $febtm = new FlightEventBookTempManager($flightEventBookTempData);
+        $febtm->addFlightEventBookTemp();
+        $flightEventBook = FlightEvent::find($flightEventBookTempData['event_id']);
+        return [
+            C::KEY_DONE => true,
+            C::KEY_RESPONSE => [
+                'flighteventbook' => [
+                    'event_id' => $flightEventBook->id,
+                    'name' => $flightEventBook->name,
+                    'country' => $flightEventBook->country,
+                    'city' => $flightEventBook->city,
+                    'location' => $flightEventBook->location,
+                    'gmLink' => $flightEventBook->gmLink,
+                    'date' => $flightEventBook->date,
+                    'image' => $flightEventBook->image,
+                    'tickets' => $flightEventBookTempData['tickets'],
+                    'price' => sprintf("%.2f",$flightEventBookTempData['price'])
+                ]
+            ]
+        ];
     }
 }
 
