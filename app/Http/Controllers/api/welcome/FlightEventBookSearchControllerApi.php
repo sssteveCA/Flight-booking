d4826a3ff086717d6a80bd3ebf58169dc4992014
= <=>
2024-01-08 22:53:04 +0000

    Added FlightEventBookSearchControllerApi

---
+++ app/Http/Controllers/api/welcome/FlightEventBookSearchControllerApi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\api\welcome;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FlightEventBookSearchControllerApi extends Controller
+{
+    //
+}
