c0474d3676ee6e7d7fa651f95c07d3007e8e6e6f
= <=>
2024-01-08 23:00:59 +0000

    Added FlightEventBookSearchControllerApi failedValidation

--- app/Http/Controllers/api/welcome/FlightEventBookSearchControllerApi.php
+++ app/Http/Controllers/api/welcome/FlightEventBookSearchControllerApi.php
@@ -13,4 +13,14 @@ class FlightEventBookSearchControllerApi extends Controller
 {
     use FlightEventBookSearchControllerCommonTrait;
 
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(
+            response()->json([
+               C::KEY_DONE => false, C::KEY_STATUS => 'ERROR', C::KEY_MESSAGE => $errors[array_key_first($errors)][0]
+            ],400,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE)
+        );
+    }
+
 }
--- app/Http/Requests/api/welcome/FlightEventPriceRequestApi.php
+++ app/Http/Requests/api/welcome/FlightEventPriceRequestApi.php
@@ -3,28 +3,19 @@
 namespace App\Http\Requests\api\welcome;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+use App\Interfaces\Constants as C;
+use App\Interfaces\Paths as P;
+use App\Traits\Common\FlightEventPriceRequestCommonTrait;
 
 class FlightEventPriceRequestApi extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+    use FlightEventPriceRequestCommonTrait;
+    
+    protected $stopOnFirstFailure = true;
+
+    
 }
