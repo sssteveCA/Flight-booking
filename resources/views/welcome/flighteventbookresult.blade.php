e03a27730dfb6d4c55407c3dfa17d4edeb98f7af
= <=>
2024-01-09 19:30:41 +0000

    Fixed flighteventbookresult.blade.php html

--- resources/views/welcome/flighteventbookresult.blade.php
+++ resources/views/welcome/flighteventbookresult.blade.php
@@ -10,7 +10,7 @@
 
 @section('links')
     @parent
-    <link rel="stylsheet" href="{{ asset('css/welcome/flighteventbookresult.scss') }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome/flighteventbookresult.css') }}">
 @endsection
 
 @section('scripts')
@@ -25,44 +25,32 @@
             @method('POST')
             <input type="hidden" name="session_id" value="{{ $response['flighteventbook']['session_id'] }}">
             <div class="flighteventbook-div">
-                <div class="flighteventbook-header bg-success bg-gradient"></div>
+                <div class="flighteventbook-header bg-success bg-gradient">Preventivo evento</div>
                 <div class="flighteventbook-row">
                     <div class="column-elem">
                         <p class="fe-ce-header bg-warning bg-gradient">Nome evento</p>
                         <p class="bg-light bg-gradient">{{ $response['flighteventbook']['name'] }}</p>
                     </div>
-                </div>
-                <div class="flighteventbook-row">
                     <div class="column-elem">
                         <p class="fe-ce-header bg-warning bg-gradient">Località</p>
                         <p class="bg-light bg-gradient">{{ $response['flighteventbook']['location'] }}</p>
                     </div>
-                </div>
-                <div class="flighteventbook-row">
                     <div class="column-elem">
                         <p class="fe-ce-header bg-warning bg-gradient">Paese</p>
                         <p class="bg-light bg-gradient">{{ $response['flighteventbook']['country'] }}</p>
                     </div>
-                </div>
-                <div class="flighteventbook-row">
                     <div class="column-elem">
                         <p class="fe-ce-header bg-warning bg-gradient">Città</p>
                         <p class="bg-light bg-gradient">{{ $response['flighteventbook']['city'] }}</p>
                     </div>
-                </div>
-                <div class="flighteventbook-row">
                     <div class="column-elem">
                         <p class="fe-ce-header bg-warning bg-gradient">Data evento</p>
                         <p class="bg-light bg-gradient">{{ $response['flighteventbook']['date'] }}</p>
                     </div>
-                </div>
-                <div class="flighteventbook-row">
                     <div class="column-elem">
                         <p class="fe-ce-header bg-warning bg-gradient">Numero biglietti</p>
                         <p class="bg-light bg-gradient">{{ $response['flighteventbook']['tickets'] }}</p>
                     </div>
-                </div>
-                <div class="flighteventbook-row">
                     <div class="column-elem">
                         <p class="fe-ce-header bg-warning bg-gradient">Prezzo totale</p>
                         <p class="bg-light bg-gradient">{{ $response['flighteventbook']['price'] }}€</p>
